f931f163593944459a2cfe7fb7b3380a80a5a574
IFTC-XLKJ <iftcceo@139.com>
2025-01-20 22:25:44 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,19 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/api", (req, res) => {
+  res.json({
+    code: 200,
+    msg: "请求成功",
+    copyright: "IFTC",
+    apis: [
+      // "GET /user/details?id={用户ID(必填)}",
+      // "GET /user/login?user={用户ID或昵称或邮箱(必填)}&password={密码(必填)}",
+      // "GET /user/register?nickname={昵称(必填)}&email={邮箱(必填)}&password={密码(必填)}&avatar={头像(选填)}",
+      // "GET /user/update?type={更新类型，包括：nickname、avatar、email、password(必填)}&id={用户ID(必填)}&password={密码(必填)}&data={要更新内容(必填)}",
+    ],
+    timestamp: time(),
+  });
 });
 
 app.listen(port, () => {
