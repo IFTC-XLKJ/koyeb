c0c8d2278bc5e76ecfb6faa096e2dabf5ca85df6
IFTC-XLKJ <iftcceo@139.com>
2025-01-21 11:28:00 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+app.use(bodyParser.json())
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
