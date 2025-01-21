44dca7b1860dc25a9a83833305394f39f721e29c
IFTC-XLKJ <iftcceo@139.com>
2025-01-21 22:02:53 +0800

    Update server.js

--- server.js
+++ server.js
@@ -58,7 +58,14 @@ app.all("/api", (req, res) => {
 
 app.get("/api/book/search", async (req, res) => {
     requestLog(req);
-    const { keyword, page, limit } = req.query;
+    const { keyword } = req.query;
+    if (keyword) { } else {
+        res.status(400).json({
+            code: 400,
+            msg: "缺少keyword参数",
+            timestamp: time(),
+        });
+    }
 })
 
 app.get("/api/runtime", (req, res) => {
