724f5711e0dd694fb2dab8801ad159e57dd97990
IFTC-XLKJ <iftcceo@139.com>
2025-01-21 11:12:41 +0800

    Update server.js

--- server.js
+++ server.js
@@ -21,6 +21,20 @@ app.get("/api", (req, res) => {
   });
 });
 
+app.post("/api/user", (req, res) => {
+  res.json({
+    code: 200,
+    msg: "请求成功",
+    copyright: "IFTC",
+    apis: [
+      "GET /user/details?id={用户ID(必填)}",
+      "GET /user/login?user={用户ID或昵称或邮箱(必填)}&password={密码(必填)}",
+      "GET /user/register?nickname={昵称(必填)}&email={邮箱(必填)}&password={密码(必)}",
+      "GET /user/update?type={更新类型，包括：nickname、avatar、email、password(必填)}&id={用户ID(必填)}&password={密码(必填)}&data={要更新内容(必填)}"
+    ]
+  })
+})
+
 app.listen(port, () => {
   console.log(`服务器已在端口 ${port} 开启`);
 });
