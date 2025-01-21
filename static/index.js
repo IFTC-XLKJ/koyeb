c443ab6e6773f84b95f4170ad43ae0c798c6ccaa
IFTC-XLKJ <iftcceo@139.com>
2025-01-21 19:30:23 +0800

    Create index.js

---
+++ static/index.js
@@ -0,0 +1,10 @@
+addEventListener("load", e => {
+    console.log("加载完成")
+    const navItems = document.querySelectorAll(".nav-item")
+    navItems.forEach(navItem => {
+        console.log(navItem)
+        navItem.addEventListener("click", e => {
+            location.href = navItem.getAttribute("iftc-page")
+        })
+    })
+})
