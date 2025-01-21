9ef606aa459a59f4bf3d4f994c272b0a6f2b4ab5
IFTC-XLKJ <iftcceo@139.com>
2025-01-21 11:56:28 +0800

    Update server.js

--- server.js
+++ server.js
@@ -161,48 +161,48 @@ app.get("/api/user/details", async (req, res) => {
             msg: "id参数类型错误，必须为数值类型",
             timestamp: time(),
         });
-  }
-  if (id) {
-    const user = new User();
-    try {
-      const json = await user.getByID(id);
-      if (json.code == 200) {
-        const data = json.fields[0];
-        if (!data) {
-          res.status(404).json({
-            code: 404,
-            id: id,
-            msg: "账号不存在",
-            timestamp: time(),
-          });
-        }
-        res.json({
-          code: 200,
-          msg: "账号数据获取成功",
-          data: {
-            ID: data.ID,
-            username: String(data.昵称),
-            avatar: data.头像,
-            VC: data.V币,
-            email: data.邮箱,
-            VIP: !!data.VIP,
-            signed: data.签到,
-            op: data.管理员 == 1,
-            freezed: data.封号 == 1,
-            title: data.头衔,
-            titleColor: data.头衔色,
-            createdAt: data.createdAt,
-            updatedAt: data.updatedAt,
-          },
-          timestamp: time(),
-        });
-      } else {
-        res.status(json.code).json({
-          code: json.code,
-          msg: json.msg,
-        });
-      }
-    } catch (e) {
+    }
+    if (id) {
+        const user = new User();
+        try {
+            const json = await user.getByID(id);
+            if (json.code == 200) {
+                const data = json.fields[0];
+                if (!data) {
+                    res.status(404).json({
+                        code: 404,
+                        id: id,
+                        msg: "账号不存在",
+                        timestamp: time(),
+                    });
+                }
+                res.json({
+                    code: 200,
+                    msg: "账号数据获取成功",
+                    data: {
+                        ID: data.ID,
+                        username: String(data.昵称),
+                        avatar: data.头像,
+                        VC: data.V币,
+                        email: data.邮箱,
+                        VIP: !!data.VIP,
+                        signed: data.签到,
+                        op: data.管理员 == 1,
+                        freezed: data.封号 == 1,
+                        title: data.头衔,
+                        titleColor: data.头衔色,
+                        createdAt: data.createdAt,
+                        updatedAt: data.updatedAt,
+                    },
+                    timestamp: time(),
+                });
+            } else {
+                res.status(json.code).json({
+                    code: json.code,
+                    msg: json.msg,
+                });
+            }
+        } catch (e) {
       res.status(500).json({
         code: 500,
         msg: "服务内部错误，请联系官方(QQ:3164417130)",
