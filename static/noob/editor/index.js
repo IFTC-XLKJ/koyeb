611be27e772560df3bb663d77264c1039f9f3dca
IFTC-XLKJ <iftcceo@139.com>
2025-01-22 21:23:00 +0800

    Update index.js

--- static/noob/editor/index.js
+++ static/noob/editor/index.js
@@ -1,3 +1,31 @@
 addEventListener("load", e => {
     console.log("加载完成")
+    window.workspace = Blockly.inject('editor', {
+        toolbox: toolbox,
+        renderer: "Zelos",
+        media: pathToMedia,
+        grid:
+        {
+            spacing: 20,
+            length: 3,
+            colour: '#ccc',
+        },
+        trashcan: false,
+        move: {
+            scrollbars: {
+                horizontal: true,
+                vertical: true
+            },
+            drag: true,
+            wheel: true
+        },
+        zoom: {
+            controls: true,
+            wheel: true,
+            maxScale: 5,
+            minScale: 0.1,
+            scaleSpeed: 1.5
+        }
+    });
+    console.log('Workspace initialized:', workspace);
 })
