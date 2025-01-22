9845eb1ef92d3d44fb7b506a521ffaea27c2e063
IFTC-XLKJ <iftcceo@139.com>
2025-01-22 21:35:01 +0800

    Update toolbox.js

--- static/noob/editor/toolbox.js
+++ static/noob/editor/toolbox.js
@@ -1,4 +1,16 @@
 const toolbox = {
     kind: "categoryToolbox",
-    contents: []
+    contents: [
+        {
+            kind: "category",
+            name: "元素",
+            colour: 100,
+            constent: [
+                {
+                    kind: "block",
+                    type: "doc_type",
+                },
+            ]
+        }
+    ]
 }
