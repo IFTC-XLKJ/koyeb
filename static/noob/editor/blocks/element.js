42680878461877a7df00f5db04e61c4224eafe50
IFTC-XLKJ <iftcceo@139.com>
2025-01-22 21:46:03 +0800

    Update element.js

--- static/noob/editor/blocks/element.js
+++ static/noob/editor/blocks/element.js
@@ -18,3 +18,20 @@ Blockly.JavaScript.forBlock['doc_type'] = function (block) {
     var code = `<!DOCTYPE ${doctype}>\n`;
     return code;
 };
+Blockly.Blocks['element_html'] = {
+    init: function () {
+        this.setNextStatement(true);
+        this.setPreviousStatement(true);
+        this.appendDummyInput()
+            .appendField('页面')
+        this.appendStatementInput('html')
+            .appendField('');
+        this.setOutput(false, "String");
+        this.setColour(160);
+    }
+};
+Blockly.JavaScript.forBlock['element_html'] = function (block) {
+    var html = Blockly.JavaScript.statementToCode(block, 'html')
+    var code = `<html>\n${html}</html>\n`;
+    return code;
+}
