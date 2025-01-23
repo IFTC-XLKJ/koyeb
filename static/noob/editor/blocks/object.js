9ed09f5f89b156472dd8208cc52526b54ec853bf
IFTC-XLKJ <iftcceo@139.com>
2025-01-23 16:36:41 +0800

    Update object.js

--- static/noob/editor/blocks/object.js
+++ static/noob/editor/blocks/object.js
@@ -27,7 +27,7 @@ Blockly.JavaScript.forBlock['element_dict'] = function (block) {
 
 Blockly.Extensions.registerMutator('dict_mutator', {
     blockMutator: true,
-    init: function () {
+    initMutator: function () {
         this.keys_ = [];
         this.values_ = [];
         this.keyConnections_ = [];
