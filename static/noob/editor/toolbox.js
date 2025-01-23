3e8e9a336694f2356adbcef1ab83d9f481a608c7
IFTC-XLKJ <iftcceo@139.com>
2025-01-23 21:32:13 +0800

    Update toolbox.js

--- static/noob/editor/toolbox.js
+++ static/noob/editor/toolbox.js
@@ -54,12 +54,10 @@ const toolbox = {
                 {
                     kind: "block",
                     type: "math_number",
-                    colour: 20,
                 },
                 {
                     kind: "block",
                     type: "text",
-                    colour: 20,
                 }
             ]
         },
@@ -84,3 +82,36 @@ const toolbox = {
         }
     ]
 }
+
+Blockly.defineBlocksWithJsonArray([
+    {
+        type: "math_number",
+        message0: "%1",
+        args0: [
+            {
+                type: "field_number",
+                name: "NUM",
+                value: 0
+            }
+        ],
+        output: "Number",
+        colour: 20,
+        tooltip: "数字",
+        helpUrl: ""
+    },
+    {
+        type: "text",
+        message0: "%1",
+        args0: [
+            {
+                type: "field_input",
+                name: "TEXT",
+                text: ""
+            }
+        ],
+        output: "String",
+        colour: 20,
+        tooltip: "文本",
+        helpUrl: ""
+    },
+]);
