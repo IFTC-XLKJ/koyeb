1121dad344bcbf28a900b8e52ffb04977804db5a
IFTC-XLKJ <iftcceo@139.com>
2025-01-25 12:41:18 +0800

    Update index.js

---
+++ static/VVMusic/index.js
@@ -0,0 +1,22 @@
+addEventListener('load', function () {
+    relayout();
+});
+
+addEventListener('resize', function () {
+    relayout();
+});
+
+function relayout() {
+    var winwidth = window.innerWidth;
+    var winheight = window.innerHeight;
+    var app = document.querySelector('#app');
+    app.style.width = winwidth + 'px';
+    app.style.height = (winheight - 90 - 25) + 'px';
+    var player = document.querySelector('#player');
+    player.style.width = winwidth + 'px';
+    player.style.height = 60 + 'px';
+    var lrc = document.getElementById('music-lrc');
+    lrc.style.width = winwidth + 'px';
+    lrc.style.height = 30 + 'px';
+    console.log(winwidth, winheight)
+}
