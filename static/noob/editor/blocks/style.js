9accf243345292bed00b626192198a246c51b991
IFTC-XLKJ <iftcceo@139.com>
2025-01-26 17:01:17 +0800

    Create style.js

---
+++ static/noob/editor/blocks/style.js
@@ -0,0 +1,26 @@
+Blockly.defineBlocksWithJsonArray([
+    {
+        type: 'style_group',
+        message0: '元素选择器 %1 %2',
+        args0: [
+            {
+                type: 'input_value',
+                name: 'STYLE',
+                check: 'String'
+            },
+            {
+                type: 'input_statement',
+                name: 'STYLE_GROUP'
+            }
+        ],
+        output: 'String',
+        colour: 230,
+        tooltip: '',
+        helpUrl: ''
+    }
+])
+Blockly.JavaScript.forBlock['style_group'] = function (block) {
+    var style = Blockly.JavaScript.valueToCode(block, 'STYLE', Blockly.JavaScript.ORDER_ATOMIC);
+    var style_group = Blockly.JavaScript.statementToCode(block, 'STYLE_GROUP');
+    return style + ' {' + style_group + '}';
+}
