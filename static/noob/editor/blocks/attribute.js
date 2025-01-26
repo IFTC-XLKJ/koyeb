d6759adf86e4fb2c4b648fb027da7ea4d3464f56
IFTC-XLKJ <iftcceo@139.com>
2025-01-26 21:20:17 +0800

    Update attribute.js

--- static/noob/editor/blocks/attribute.js
+++ static/noob/editor/blocks/attribute.js
@@ -1,8 +1,27 @@
 Blockly.defineBlocksWithJson([
     {
         type: "attr_get",
-        message0: "",
+        message0: "%1",
         args0: [
+            {
+                type: "field_dropdown",
+                name: "ATTRIBUTE",
+                options: [
+                    ["class", "class"],
+                    ["id", "id"],
+                    ["style", "style"],
+                    ["src", "src"],
+                    ["href", "href"],
+                    ["alt", "alt"],
+                    ["title", "title"],
+                    ["target", "target"],
+                    ["type", "type"],
+                    ["value", "value"],
+                ]
+            }
         ]
     }
 ])
+Blockly.JavaScript.forBlock["attr_get"] = function (block) {
+    return [`${block.getFieldValue("ATTRIBUTE")}`, Blockly.JavaScript.ORDER_NONE]
+}
