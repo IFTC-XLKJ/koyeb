a03a570c2e495e6002e013ed156463b2643829d0
IFTC-XLKJ <iftcceo@139.com>
2025-01-27 12:37:07 +0800

    Update style.js

--- static/noob/editor/blocks/style.js
+++ static/noob/editor/blocks/style.js
@@ -68,8 +68,30 @@ Blockly.defineBlocksWithJsonArray([
         inputsInline: true,
         nextStatement: true,
         previousStatement: true,
+    },
+    {
+        type: "style_font_size",
+        message0: '字体大小 %1',
+        args0: [
+            {
+                type: 'field_input',
+                name: 'SIZE',
+                text: '16px'
+            }
+        ],
+        colour: 230,
+        tooltip: '',
+        helpUrl: '',
+        inputsInline: true,
+        nextStatement:true,
+        previousStatement: true,
     }
 ]);
+
+Blockly.JavaScript.forBlock['style_font_size'] = function (block) {
+    var size = block.getFieldValue('SIZE');
+    return `font-size: ${size};\n`;
+};
 
 Blockly.JavaScript.forBlock['style_background_color'] = function (block) {
     var color = block.getFieldValue('COLOR');
