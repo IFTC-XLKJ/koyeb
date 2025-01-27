441d06a5c2e2fd405541fd001639dc6a9de0d09c
IFTC-XLKJ <iftcceo@139.com>
2025-01-27 16:07:47 +0800

    Update operation.js

--- static/noob/editor/blocks/operation.js
+++ static/noob/editor/blocks/operation.js
@@ -47,8 +47,49 @@ Blockly.defineBlocksWithJsonArray([
         tooltip: "拼接文本",
         helpUrl: "",
         inputsInline: true
+    },
+    {
+        type: "num_operator",
+        message0: "%1 %2 %3",
+        args0: [
+            {
+                type: "input_value",
+                name: "NUM0",
+                check: "Number"
+            },
+            {
+                type: "field_dropdown",
+                name: "OP",
+                options: [
+                    ["+", "+"],
+                    ["-", "-"],
+                    ["×", "*"],
+                    ["÷", "/"],
+                    ["%", "%"],
+                    ["^", "**"]
+                ]
+            },
+            {
+                type: "input_value",
+                name: "NUM1",
+                check: "Number"
+            }
+        ],
+        output: "Number",
+        colour: 20,
+        tooltip: "数字运算",
+        helpUrl: "",
+        inputsInline: true
     }
 ]);
+
+Blockly.JavaScript.forBlock['num_operator'] = function (block) {
+    var operator = block.getFieldValue('OP');
+    var num0 = Blockly.JavaScript.valueToCode(block, 'NUM0', Blockly.JavaScript.ORDER_ATOMIC);
+    var num1 = Blockly.JavaScript.valueToCode(block, 'NUM1', Blockly.JavaScript.ORDER_ATOMIC);
+    return ["(" + num0 + operator + num1 + ")", Blockly.JavaScript.ORDER_ATOMIC];
+}
+
 Blockly.JavaScript.forBlock['text'] = function (block) {
     var text = block.getFieldValue('TEXT');
     return ['"' + text + '"', Blockly.JavaScript.ORDER_ATOMIC];
