a33352b12c65003c8a54353fadcb65cb83e3829e
IFTC-XLKJ <iftcceo@139.com>
2025-01-30 15:44:53 +0800

    Update NOOB.js

--- NOOB.js
+++ NOOB.js
@@ -1,3 +1,8 @@
 const fetch = require("node-fetch");
 const Sign = require("./Sign.js");
 const sign = new Sign();
+
+const NOOBKey = "LkduYVIN+ZUTA2pX6P23IcX+TZ/pbiiMk08tX2/Rm0dbeqAqR82HeOjnd+soDEpbSbW06EwVYT38wb0nNOx5lxTmPkmVBOErbF5mNqsyQOjtpix5Xz10DuRd2uyWwJKst8iKDzWqQWSRSQcB1kDw0lDy+GZKmxv0o15DuJZVFC0=";
+const getDataURL = "https://api.pgaot.com/dbs/cloud/get_table_data";
+const setDataURL = "https://api.pgaot.com/dbs/cloud/set_table_data";
+const contentType = "application/json";
