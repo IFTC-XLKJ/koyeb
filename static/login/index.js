af7abe7c100d1168245754de5fd603caa1f51923
IFTC-XLKJ <iftcceo@139.com>
2025-02-11 12:07:24 +0800

    Update index.js

--- static/login/index.js
+++ static/login/index.js
@@ -1,7 +1,25 @@
+const { response } = require("express");
+
 const loginForm = document.getElementById("loginForm");
 const user = document.getElementById("user");
 const password = document.getElementById("password");
 
 loginForm.addEventListener("submit", async (e) => {
     e.preventDefault();
+    try {
+        const response = await fetch(`/api/user/login?user=${encodeURIComponent(user.value)}&password=${encodeURIComponent(password.value)}`);
+        const data = await response.json();
+        if (response.ok) {
+            if (data.code == 200) {
+                alert("登录成功");
+                location.href = "/";
+            } else {
+                alert("登录失败，原因：" + data.msg);
+            }
+        } else {
+            alert("登录失败，原因：" + data.msg);
+        }
+    } catch (error) {
+        alert("登录失败，原因：" + error);
+    }
 });
