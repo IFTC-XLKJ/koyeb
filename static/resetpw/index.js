987b85f6172708fe8c400b14bd2cafffb7cbf8fb
IFTC-XLKJ <iftcceo@139.com>
2025-02-11 13:41:15 +0800

    Update index.js

--- static/resetpw/index.js
+++ static/resetpw/index.js
@@ -1,9 +1,3 @@
 const resetpwForm = document.getElementById("resetpwForm");
 const user = document.getElementById("user");
 const password = document.getElementById("password");
-
-if (localStorage.getItem("ID") && localStorage.getItem("password")) {
-    const url = new URL(location.href);
-    const page = url.searchParams.get("page") || "/";
-    location.href = page;
-}
