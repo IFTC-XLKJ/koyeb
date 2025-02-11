b170f8f8287f6b3abe54a020eae68c310ebdde90
IFTC-XLKJ <iftcceo@139.com>
2025-02-11 10:21:41 +0800

    Update index.js

--- static/signup/index.js
+++ static/signup/index.js
@@ -1,5 +1,7 @@
 const registerForm = document.getElementById("registerForm");
 const user = document.getElementById("user");
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirmPassword");
 
 registerForm.addEventListener("submit", async (e) => {
 });
