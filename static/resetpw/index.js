233046015f6c8e41ef52e238d31ed942ce274571
IFTC-XLKJ <iftcceo@139.com>
2025-02-11 13:51:24 +0800

    Update index.js

--- static/resetpw/index.js
+++ static/resetpw/index.js
@@ -6,4 +6,20 @@ const password = document.getElementById("password");
 resetpwForm.addEventListener("submit", async (e) => {
     e.preventDefault();
     globalThis.password = password.value;
+    try {
+        const response = await fetch(`/api/user/resetpassword?email=${encodeURIComponent(email.value.trim())}&ID=${encodeURIComponent(ID.value)}&password=${encodeURIComponent(password.value)}`);
+        if (response.ok) {
+            const data = await response.json();
+            if (data.code == 200) {
+                alert("重置密码的邮件已发送，请检查邮箱");
+                window.location.href = "/login";
+            } else {
+                alert("重置密码失败，原因：" + data.message);
+            }
+        } else {
+            alert("重置密码失败，原因：" + response.statusText);
+        }
+    } catch (error) {
+        alert("重置密码失败，原因：" + error);
+    }
 });
