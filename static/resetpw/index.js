027ad4d9436bd3f45b78cabfdfb589068f3a40ce
IFTC-XLKJ <iftcceo@139.com>
2025-02-11 13:43:13 +0800

    Update index.js

--- static/resetpw/index.js
+++ static/resetpw/index.js
@@ -1,3 +1,7 @@
 const resetpwForm = document.getElementById("resetpwForm");
 const user = document.getElementById("user");
 const password = document.getElementById("password");
+
+resetpwForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+});
