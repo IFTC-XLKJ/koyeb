b74a18340fa0d1b3f2ed48b6212ff0fd9be8f848
IFTC-XLKJ <151902522+IFTC-XLKJ@users.noreply.github.com>
2025-02-13 16:51:09 +0800

    更新 server.js

--- server.js
+++ server.js
@@ -180,6 +180,12 @@ app.get("/noob/share/:workId", async (req, res) => {
     const { workId } = req.params;
 })
 
+app.get('/proxy/*', (req, res) => {
+    const requestedPath = req.url；
+    const url = requestedPath.replace("/proxy/", "");
+    res.json({url})
+});
+
 app.all("/BingSiteAuth.xml", (req, res) => {
     requestLog(req);
     res.sendFile(path.join(__dirname, "BingSiteAuth.xml"));
