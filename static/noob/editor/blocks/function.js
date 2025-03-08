490458a1b3c4c4b893fc7d46f2f69a9a5c93ccb7
IFTC-XLKJ <iftcceo@139.com>
2025-03-08 18:18:37 +0800

    Update function.js

---
+++ static/noob/editor/blocks/function.js
@@ -0,0 +1,47 @@
+Blockly.defineBlocksWithJsonArray([
+    {
+        type: 'function',
+        message0: '函数 %1 %2',
+        args0: [
+            {
+                type: 'field_input',
+                name: 'NAME',
+                text: 'func'
+            },
+            {
+                type: 'input_statement',
+                name: 'STACK'
+            }
+        ],
+        colour: 290,
+        tooltip: '函数',
+        helpUrl: '',
+        nextStatement: true,
+        previousStatement: true,
+    },
+    {
+        type: 'function_call',
+        message0: '调用函数 %1',
+        args0: [
+            {
+                type: 'field_input',
+                name: 'NAME',
+                text: 'func'
+            }
+        ],
+        colour: 290,
+        tooltip: '调用函数',
+        helpUrl: '',
+        inputsInline: true,
+        output: null,
+    }
+])
+Blockly.JavaScript['function'] = function (block) {
+    var name = block.getFieldValue('NAME');
+    var stack = Blockly.JavaScript.statementToCode(block, 'STACK');
+    return `function ${name}() {\n${stack}\n}\n`;
+}
+Blockly.JavaScript['function_call'] = function (block) {
+    var name = block.getFieldValue('NAME');
+    return `${name}();\n`;
+}
