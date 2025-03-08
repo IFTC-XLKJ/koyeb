87a29fea6c2108dede8ee1ac9dfbf473fd6781cc
IFTC-XLKJ <iftcceo@139.com>
2025-03-08 18:29:03 +0800

    Update document.js

---
+++ static/noob/editor/blocks/document.js
@@ -0,0 +1,10 @@
+Blockly.defineBlocksWithJsonArray([
+    {
+        type: "get_document",
+        message0: "文档",
+        output: "Dictionary",
+        colour: "#F8AA87",
+        tooltip: "获取文档",
+        helpUrl: ""
+    }
+])
