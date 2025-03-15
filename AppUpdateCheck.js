1c813fd5ee496ea4894f199faaed50956875c993
IFTC-XLKJ <iftcceo@139.com>
2025-03-15 12:57:19 +0800

    Update AppUpdateCheck.js

--- AppUpdateCheck.js
+++ AppUpdateCheck.js
@@ -9,7 +9,41 @@ const contentType = "application/json";
 
 class AppUpdateCheck {
     constructor() { }
+    /**
+     * 检查更新
+     * @param {String} packageName 
+     * @param {Number} versionCode 
+     */
     async check(packageName, versionCode) {
+        const timestamp = time();
+        const signaturePromise = sign.get(timestamp);
+        try {
+            const signature = await signaturePromise;
+            const response = await fetch(getDataURL, {
+                method: "POST",
+                headers: {
+                    "X-Pgaot-Key": AppUpdateCheckKey,
+                    "X-Pgaot-Sign": signature,
+                    "X-Pgaot-Time": timestamp.toString(),
+                    "Content-Type": contentType
+                },
+                body: JSON.stringify({
+                    filter: `应用包名='${packageName}' AND 版本号>${versionCode}`,
+                    page: 1,
+                    limit: 1,
+                })
+            });
+            const json = await response.json();
+            console.log(json);
+            if (json.code === 200 && json.data.length > 0) {
+                return json.data[0];
+            } else {
+                return null;
+            }
+        } catch (error) {
+            console.error('There was a problem with the fetch operation:', error);
+            throw error;
+        }
     }
 }
 
