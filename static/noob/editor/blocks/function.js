50643ae2da36b335a7ed8c39c4554e48710bfe48
IFTC-XLKJ <iftcceo@139.com>
2025-04-05 21:18:17 +0800

    Update function.js

--- static/noob/editor/blocks/function.js
+++ static/noob/editor/blocks/function.js
@@ -131,6 +131,10 @@ Blockly.JavaScript.forBlock['function'] = function (block) {
     var stack = Blockly.JavaScript.statementToCode(block, 'STACK');
     return `async function ${name}(${paramCode}) {\n${stack}\n}\n`;
 }
+Blockly.JavaScript.forBlock['function_param'] = function (block) {
+    var name = block.getFieldValue('NAME');
+    return `funparam_${name}`;
+}
 Blockly.JavaScript.forBlock['function_call'] = function (block) {
     var name = block.getFieldValue('NAME');
     return `${name}();\n`;
