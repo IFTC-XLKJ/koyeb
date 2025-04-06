01b8d71e874d5fa7814a0dc294518050c0d53134
IFTC-XLKJ <iftcceo@139.com>
2025-04-06 15:19:19 +0800

    Update Discussion.js

--- Discussion.js
+++ Discussion.js
@@ -1,3 +1,16 @@
-class Discussion {}
+const fetch = require("node-fetch");
+const Sign = require("./Sign.js");
+const sign = new Sign();
+
+const VVDiscussion = "LkduYVIN+ZW+aXatg3bnYupOvfl2DlTOk08tX2/Rm0dbeqAqR82HeOjnd+soDEpbSbW06EwVYT38wb0nNOx5lxTmPkmVBOErbF5mNqsyQOjMsGiImi0mbsxAI7TJNkqiqEaLlJXoSc0IahckjU0GMxUf3ahzRxkmo15DuJZVFC0=";
+const getDataURL = "https://api.pgaot.com/dbs/cloud/get_table_data";
+const setDataURL = "https://api.pgaot.com/dbs/cloud/set_table_data";
+const contentType = "application/json";
+
+class Discussion {
+    get(page) {
+
+    }
+}
 
 export default Discussion;
