6f1ee094376e8fe2d8156dd705f4cbc29d06d08e
IFTC-XLKJ <151902522+IFTC-XLKJ@users.noreply.github.com>
2025-04-16 07:46:47 +0800

    update server.js

--- server.js
+++ server.js
@@ -309,6 +309,7 @@ app.all("/api", (req, res) => {
         "获取NOOB作品 GET /noob/works?id={用户ID(必填)}&password={密码(必填)}",
         "应用更新检查 GET /appupdatecheck?packageName={应用包名}&versionCode={版本号}",
         "请求记录 GET /requestips",
+        "获取论坛 GET /discussion/get?page={页数，每页返回10条(必填)}",
     ]
     res.json({
         code: 200,
