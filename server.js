504155076bce44bbbbadf5be6d1956670bc2ca62
IFTC-XLKJ <151902522+IFTC-XLKJ@users.noreply.github.com>
2025-04-21 21:12:12 +0800

    update server.js

--- server.js
+++ server.js
@@ -573,7 +573,12 @@ app.get('/api/bot/user/login', (req, res) => {
     const UUIDdb = new UUID_db();
     try {
         const json = await UUIDdb.getData();
-        if (json.code == 200) {} else {
+        if (json.code == 200) {
+            const data = json.fields[0];
+            if (!data) {
+                res.status(400).json({})
+            }
+        } else {
             res.status(json.code).json({
                 code: json.code,
                 msg: json.msg,
