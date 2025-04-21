33d967799b4ef87d2fe4db4c3653734663e6df81
IFTC-XLKJ <151902522+IFTC-XLKJ@users.noreply.github.com>
2025-04-22 00:06:19 +0800

    update static/bindqq/index.js

--- static/bindqq/index.js
+++ static/bindqq/index.js
@@ -8,7 +8,7 @@ bindqqForm.addEventListener("submit", async (e) => {
     e.preventDefault();
     e.preventDefault();
     try {
-        const response = await fetch(`/api/bindqq?ID=${user.value}&QQ=${qq.value)}&password=${encodeURIComponent(password.value)}`);
+        const response = await fetch(`/api/bindqq?ID=${user.value}&QQ=${qq.value}&password=${encodeURIComponent(password.value)}`);
         const data = await response.json();
         if (response.ok) {
             if (data.code == 200) {
@@ -18,15 +18,16 @@ bindqqForm.addEventListener("submit", async (e) => {
                     await navigator.clipboard.writeText(`/登录 ${uuid}`);
                     console.log('复制成功！');
                     alert("指令自动复制成功")
-                } catch (err) {
+                } catch(err) {
                     console.error('复制失败：', err);
                     alert("指令自动复制失败，原因：" + err.message);
                 }
             } else {
                 alert("请求绑定QQ失败，原因：" + data.msg);
             }
+        }
     } catch(e) {
         alert(`请求绑定QQ出错：${e.message}`);
         console.log(e);
     }
-})
+});
