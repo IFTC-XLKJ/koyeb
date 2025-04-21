6172760dd3cce767635790ee24e95ed706a906fc
IFTC-XLKJ <151902522+IFTC-XLKJ@users.noreply.github.com>
2025-04-21 23:45:08 +0800

    update static/bindqq/index.js

--- static/bindqq/index.js
+++ static/bindqq/index.js
@@ -8,7 +8,13 @@ bindqqForm.addEventListener("submit", async e => {
     try {
         const response = await fetch(`/api/bindqq?ID=${user.value}&QQ=${qq.value)}&password=${encodeURIComponent(password.value)}`);
         const data = await response.json();
-        
+        if (response.ok) {
+            if (data.code == 200) {
+                const uuid = data.uuid;
+                alert(`请求绑定QQ成功，请加入官群(870350184)，并通过 @VV助手 /登录 ${uuid}`);
+            } else {
+                alert("请求绑定QQ失败，原因：" + data.msg);
+            }
     } catch(e) {
         alert(`请求绑定QQ出错：${e.message}`);
         console.log(e);
