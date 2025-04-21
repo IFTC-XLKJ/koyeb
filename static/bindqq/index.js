7016d9a1b5d01534982eb5af8cc8a5cf805b76f5
IFTC-XLKJ <151902522+IFTC-XLKJ@users.noreply.github.com>
2025-04-21 22:59:07 +0800

    update static/bindqq/index.js

--- static/bindqq/index.js
+++ static/bindqq/index.js
@@ -2,3 +2,7 @@ const bindqqForm = document.getElementById("bindqqForm");
 const user = document.getElementById("user");
 const password = document.getElementById("password");
 const qq = document.getElementById("qq");
+
+bindqqForm.addEventListener("submit", async e => {
+    e preventDefault();
+})
