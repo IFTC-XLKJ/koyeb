8c7d0e71c9843ccdb12ad81d20f10c37c1d8084d
IFTC-XLKJ <151902522+IFTC-XLKJ@users.noreply.github.com>
2025-04-21 22:56:44 +0800

    update static/bindqq/index.js

---
+++ static/bindqq/index.js
@@ -0,0 +1,4 @@
+const bindqqForm = document.getElementById("bindqqForm");
+const user = document.getElementById("user");
+const password = document.getElementById("password");
+const qq = document.getElementById("qq");
