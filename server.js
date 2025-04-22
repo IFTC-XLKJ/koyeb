4cbe866d133dc1f0e929ceb89181c5e47b8fbb30
IFTC-XLKJ <151902522+IFTC-XLKJ@users.noreply.github.com>
2025-04-22 22:12:08 +0800

    update server.js

--- server.js
+++ server.js
@@ -350,7 +350,9 @@ app.all("/api", (req, res) => {
     });
 });
 
-app.get("/api/bookshelf/get", async (req, res) => {});
+app.get("/api/bookshelf/get", async (req, res) => {
+    requestLog(req);
+});
 
 app.get("/api/bindqq", async (req, res) => {
     requestLog(req);
