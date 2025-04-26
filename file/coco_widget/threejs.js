1e581c7c27e95c7a43009de1dab696d8edbc584f
IFTC-XLKJ <iftcceo@139.com>
2025-04-26 13:40:15 +0800

    Update threejs.js

--- file/coco_widget/threejs.js
+++ file/coco_widget/threejs.js
@@ -70,7 +70,7 @@ class Widget extends VisibleWidget {
         }
         Object.assign(this, props);
         this._canvas = null;
-        importScript("https://cdnjs.cloudflare.com/ajax/libs/three.js/0.174.0/three.tsl.js", () => {
+        importScript("https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js", () => {
             this.emit("scriptLoad");
             this.scriptLoaded = () => {
                 return true;
