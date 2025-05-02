be4106fb4476d933dff5b91753597c50bcda2186
IFTC-XLKJ <iftcceo@139.com>
2025-05-02 19:01:56 +0800

    Create vvcomponent.js

---
+++ static/noob/editor/blocks/vvcomponent.js
@@ -0,0 +1,12 @@
+Blockly.defineBlocksWithJsonArray([
+    {
+        type: "vv_dragger",
+        message0: "拖拽器 元素选择器%1",
+        args0: [
+            {
+                type: "input_value",
+                name: "",
+            }
+        ],
+    }
+]);
