4642047fef2fb35adb381b6af7f4a31f02a90c6b
IFTC-XLKJ <iftcceo@139.com>
2025-05-03 22:13:00 +0800

    Update cloudfun.js

--- file/cloudfun.js
+++ file/cloudfun.js
@@ -1,5 +1,10 @@
 async function handle(request) {
-    return new request.response();
+    return new request.response(JSON.stringify({
+        code: 200,
+        msg: "Hello World",
+    }), {
+        status: 200,
+});
 }
 
 module.exports = handle;
