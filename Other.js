80b2d725c6a89e127dd2d3ee9e68167b34709a6f
IFTC-XLKJ <iftcceo@139.com>
2025-05-04 14:57:47 +0800

    Update Other.js

--- Other.js
+++ Other.js
@@ -51,7 +51,13 @@ class Other {
                         const src = data.数据;
                         const response = await fetch(src);
                         const code = await response.text();
-                        const fun = eval(`globalThis.require = null;var require = null;\n${code}`);
+                        const fun = eval(`const fetch = require("node-fetch");
+                        globalThis.require = null;
+                        var require = async function(src) {
+                            const response = await fetch(src);
+                            const code = await response.text();
+                            return eval(code);
+                        };\n${code}`);
                         console.log(fun, typeof fun);
                         const request = {
                             response: class {
@@ -87,7 +93,6 @@ class Other {
                             headers: req.headers,
                             UUID: uuid,
                             tools: {
-                                fetch: fetch,
                             },
                         };
                         fun(request);
