06b8b05f71690dd4574f8edcaa21fad9caa90011
IFTC-XLKJ <iftcceo@139.com>
2025-05-05 20:02:15 +0800

    Create CloudfunConsole.js

---
+++ CloudfunConsole.js
@@ -0,0 +1,73 @@
+const fetch = require("node-fetch");
+const Sign = require("./Sign.js");
+const sign = new Sign();
+
+const ConsoleKey = "LkduYVIN+ZV9wMyAV9CO3a9h7A1kYvFOk08tX2/Rm0dbeqAqR82HeOjnd+soDEpbSbW06EwVYT38wb0nNOx5lxTmPkmVBOErbF5mNqsyQOi31cRK2y7Ag4Z8S1HVu+YVfc9eV+c+nKBGeuZzKOdkZYPHlIL1yiJto15DuJZVFC0="
+const getDataURL = "https://api.pgaot.com/dbs/cloud/get_table_data";
+const setDataURL = "https://api.pgaot.com/dbs/cloud/set_table_data";
+const contentType = "application/json";
+
+class CloudfunConsole {
+    constructor() { }
+    async getLogs(uuid) {
+        const timestamp = Date.now();
+        const signaturePromise = sign.get(timestamp);
+        try {
+            const signature = await signaturePromise;
+            const response = await fetch(getDataURL, {
+                method: "POST",
+                headers: {
+                    "X-Pgaot-Key": ConsoleKey,
+                    "X-Pgaot-Sign": signature,
+                    "X-Pgaot-Time": timestamp.toString(),
+                    "Content-Type": contentType,
+                },
+                body: JSON.stringify({
+                    filter: `UUID="${uuid}"`,
+                    page: 1,
+                    limit: 1000,
+                }),
+            });
+            if (!response.ok) {
+                throw new Error("Network response was not ok " + response.statusText);
+            }
+            const json = await response.json();
+            console.log(json);
+            return json;
+        } catch (error) {
+            console.error("There was a problem with the fetch operation:", error);
+            throw error;
+        }
+    }
+    async setLogs(uuid, type, log) {
+        const timestamp = Date.now();
+        const signaturePromise = sign.get(timestamp);
+        try {
+            const response = await fetch(setDataURL, {
+                method: "POST",
+                headers: {
+                    "X-Pgaot-Key": ConsoleKey,
+                    "X-Pgaot-Sign": signature,
+                    "X-Pgaot-Time": timestamp.toString(),
+                    "Content-Type": contentType,
+                },
+                body: JSON.stringify({
+                    type: "INSERT",
+                    filter: `UUID,类型,内容`,
+                    fields: `("${uuid}", "${type}", "${log}")`,
+                })
+            })
+            if (!response.ok) {
+                throw new Error("Network response was not ok " + response.statusText);
+            }
+            const json = await response.json();
+            console.log(json);
+            return json;
+        } catch (error) {
+            console.error('There was a problem with the fetch operation:', error);
+            throw error;
+        }
+    }
+}
+
+module.exports = UUID_db;
