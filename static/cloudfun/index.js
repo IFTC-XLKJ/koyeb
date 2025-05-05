fe4b1dde954d3c40f9f72077daac17aa85f87c42
IFTC-XLKJ <iftcceo@139.com>
2025-05-05 11:46:11 +0800

    Update index.js

--- static/cloudfun/index.js
+++ static/cloudfun/index.js
@@ -2,7 +2,13 @@ const toast = new Toast();
 create.addEventListener("click", async () => {
     const loadid = toast.showToast("正在创建云函数", 0, "center", "small", "loading", false, false);
     if (localStorage.getItem("ID") && localStorage.getItem("password")) {
-
+        const input = document.createElement("input");
+        input.type = "file";
+        input.accept = ".js";
+        input.addEventListener("change", async e => {
+        });
+        input.click();
+        // const json = await fetch(`https://iftc.koyeb.app/api/cloudfun/new?ID=${localStorage.getItem("ID")}&password=${encodeURIComponent(localStorage.getItem("password"))}&file=${}}`);
     } else {
         toast.hideToast(loadid);
         toast.showToast("请先登录", 2, "center", "small", "error", false, true);
