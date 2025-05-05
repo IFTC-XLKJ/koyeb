5080c5095ce554d46732468eab95b2def1ed7964
IFTC-XLKJ <iftcceo@139.com>
2025-05-05 15:55:53 +0800

    Update UUID_db.js

--- UUID_db.js
+++ UUID_db.js
@@ -181,5 +181,27 @@ class UUID_db {
             throw error;
         }
     }
+    async update(ID, data) {
+        const timestamp = Date.now();
+        const signaturePromise = sign.get(timestamp);
+        try {
+            const signature = await signaturePromise;
+            const response = await fetch(setDataURL, {
+                method: "POST",
+                headers: {
+                    "X-Pgaot-Key": VVZHkey,
+                    "X-Pgaot-Sign": signature,
+                    "X-Pgaot-Time": timestamp.toString(),
+                    "Content-Type": contentType,
+                },
+                body: JSON.stringify({
+                    type: "UPDATE",
+                })
+            })
+        } catch (error) {
+            console.error("There was a problem with the fetch operation:", error);
+            throw error;
+        }
+    }
 }
 module.exports = UUID_db;
