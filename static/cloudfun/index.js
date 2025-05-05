0e399c19b722f1eae0609a88b70561966f277eee
IFTC-XLKJ <iftcceo@139.com>
2025-05-05 11:24:49 +0800

    Update index.js

--- static/cloudfun/index.js
+++ static/cloudfun/index.js
@@ -1,6 +1,8 @@
+const toast = new Toast();
 create.addEventListener("click", async () => {
-    if (localStorage.getItem("ID") && localStorage.getItem("password")) {} else {
-        alert("Please login first!");
+    if (localStorage.getItem("ID") && localStorage.getItem("password")) {
+
+    } else {
         return;
     }
 });
