aafa64614549f23b65461abe185f940fc09382d0
IFTC-XLKJ <iftcceo@139.com>
2025-05-05 11:32:28 +0800

    Update index.js

--- static/cloudfun/index.js
+++ static/cloudfun/index.js
@@ -1,9 +1,10 @@
 const toast = new Toast();
 create.addEventListener("click", async () => {
-    toast.showToast("正在创建云函数", 0, "center", "small", "loadding", null, true);
+    const loadid = toast.showToast("正在创建云函数", 0, "center", "small", "loadding", null, true);
     if (localStorage.getItem("ID") && localStorage.getItem("password")) {
 
     } else {
+        toast.hideToast(loadid);
         toast.showToast("请先登录", 2, "center", "small", "error", null, true);
         return;
     }
