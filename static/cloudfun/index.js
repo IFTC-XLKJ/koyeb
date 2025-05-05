d41d9040e84c5aa9b1c50132618c9295ca9e690c
IFTC-XLKJ <iftcceo@139.com>
2025-05-05 11:25:04 +0800

    Update index.js

--- static/cloudfun/index.js
+++ static/cloudfun/index.js
@@ -3,6 +3,7 @@ create.addEventListener("click", async () => {
     if (localStorage.getItem("ID") && localStorage.getItem("password")) {
 
     } else {
+        toast.showToast("请先登录", 1, "error", "custom", 0, 0, 0, 0, 0, 0, 0, 0);
         return;
     }
 });
