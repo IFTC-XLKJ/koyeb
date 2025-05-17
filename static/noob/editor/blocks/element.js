ef2465859892f014a1b1db3ca4ba7725c8450f69
IFTC-XLKJ <iftcceo@139.com>
2025-05-17 15:15:07 +0800

    Update element.js

--- static/noob/editor/blocks/element.js
+++ static/noob/editor/blocks/element.js
@@ -60,7 +60,7 @@ Blockly.Blocks['element_head'] = {
 };
 Blockly.JavaScript.forBlock['element_head'] = function (block) {
     var html = Blockly.JavaScript.statementToCode(block, 'html')
-    var code = `<head iftc-annotation="NOOB编辑器">\n${html}</head>\n`;
+    var code = `<head iftc-annotation="NOOB编辑器">\n<script src="/file/component/Dragger.js"></script>${html}</head>\n`;
     return code;
 }
 // 主体
