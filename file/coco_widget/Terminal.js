3a26dadddba0e6140ea9e065c15694a01e4a52ce
IFTC-XLKJ <iftcceo@139.com>
2025-05-25 20:18:30 +0800

    114514

---
+++ file/coco_widget/Terminal.js
@@ -0,0 +1,107 @@
+var { window, document } = this;
+var { fetch, console } = window;
+
+const METHOD_COLOR = '#1E90FF';
+
+function importScript(src, load, error) {
+    const script = document.createElement("script");
+    script.src = src;
+    script.onload = load;
+    script.onerror = error;
+    document.head.appendChild(script);
+}
+const types = {
+    isInvisibleWidget: false,
+    type: "TERMINAL",
+    icon: "https://iftc.koyeb.app/static/terminal.svg",
+    title: "虚拟终端",
+    version: "1.0.0",
+    isGlobalWidget: false,
+    properties: [
+        {
+            key: '__width',
+            label: '宽度',
+            valueType: 'number',
+            defaultValue: 200,
+            blockOptions: {
+                generateBlock: false,
+            },
+        },
+        {
+            key: '__height',
+            label: '高度',
+            valueType: 'number',
+            defaultValue: 150,
+            blockOptions: {
+                generateBlock: false,
+            },
+        },
+        {
+            key: '__size',
+            label: '',
+            valueType: 'number',
+            defaultValue: 0,
+            readonly: true,
+            blockOptions: {
+                setter: {
+                    keys: ['__height', '__width'],
+                },
+                getter: {
+                    keys: ['__height', '__width'],
+                },
+            },
+        },
+    ],
+    methods: [
+        {
+            key: 'isLoad',
+            label: '是否加载完成',
+            params: [],
+            valueType: 'boolean',
+            blockOptions: {
+                callMethodLabel: false,
+                color: METHOD_COLOR,
+            },
+        }
+    ],
+    events: [],
+};
+
+class Widget extends VisibleWidget {
+    constructor(props) {
+        super(props);
+        Object.assign(this, props);
+        this.id = this.widgetId + "_TERMINAL";
+        this.terminal = null;
+        importScript(
+            "https://cdnjs.cloudflare.com/ajax/libs/xterm/5.5.0/xterm.js",
+            () => {
+                this.terminal = new Terminal({
+                    cursorBlink: true,
+                    fontSize: 14,
+                    theme: {
+                        background: '#000000',
+                        foreground: '#FFFFFF',
+                    },
+                });
+                console.log(this.terminal);
+                // this.terminal.open(document.getElementById(this.id));
+                // this.terminal.write('Welcome to the virtual terminal!\r\n');
+            },
+            () => {
+                console.error("Failed to load xterm.js");
+            }
+        );
+    }
+    render() {
+        // return (
+        //     <div id={this.widgetId + "_TERMINAL"}></div>
+        // );
+    }
+    isLoad() {
+        return !!this.terminal;
+    }
+}
+
+exports.types = types;
+exports.widget = Widget;
