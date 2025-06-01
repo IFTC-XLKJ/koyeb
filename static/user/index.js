2a4dffedf48a166866cc7fc92de08a1543352a41
IFTC-XLKJ <iftcceo@139.com>
2025-06-01 16:31:36 +0800

    Update index.js

--- static/user/index.js
+++ static/user/index.js
@@ -1,3 +1,5 @@
+const { isTypePredicateNode } = require("typescript");
+
 const userId = localStorage.getItem("ID");
 if (!userId) location.href = "/login";
 if (parserUrlParams().id != userId) location.href = `/user?id=${userId}`;
@@ -8,13 +10,23 @@ updateAvatar.addEventListener("click", e => {
     const fileSelector = document.createElement("input");
     fileSelector.type = "file";
     fileSelector.accept = ".jpg,.jpeg,.png,.gif,.webp,.svg";
-    fileSelector.addEventListener("change", e => {
+    fileSelector.addEventListener("change", async e => {
         const file = e.target.files[0];
         if (file.size > 1024 * 1024 * 25) return alert("文件大小不能超过25MB");
         if (file) {
             const formData = new FormData();
-            formData.append("file", file);
-            formData.append("id", userId);
+            formData.append("file", file, "avatar.png");
+            formData.append("path", "vv/avatar");
+            const requestOptions = {
+                method: 'POST',
+                body: formdata,
+                redirect: 'follow'
+            }; try {
+                const response = await fetch("https://api.pgaot.com/user/up_cat_file", requestOptions);
+                const data = await response.json();
+            } catch (e) {
+                alert("上传头像失败，原因：" + e);
+            }
         }
     });
     fileSelector.click();
