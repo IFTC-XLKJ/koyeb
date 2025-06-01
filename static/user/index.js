ad4c7322e5aff4dc62d8d9ce2fe1d7bb6f88dd14
IFTC-XLKJ <iftcceo@139.com>
2025-06-01 16:25:23 +0800

    666

--- static/user/index.js
+++ static/user/index.js
@@ -3,6 +3,16 @@ if (!userId) location.href = "/login";
 if (parserUrlParams().id != userId) location.href = `/user?id=${userId}`;
 if (!parserUrlParams().id) location.href = `/user?id=${userId}`;
 
+const updateAvatar = document.getElementById("update-avatar");
+updateAvatar.addEventListener("click", e => {
+    const file = document.createElement("input");
+    file.type = "file";
+    file.accept = ".jpg,.jpeg,.png,.gif,.webp,.svg";
+    file.addEventListener("change", e => {
+    });
+    file.click();
+});
+
 function parserUrlParams() {
     const params = new URLSearchParams(location.search);
     const result = {};
