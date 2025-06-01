b4eb6d7c8940581114a4fadd1cce462b7b631c6c
IFTC-XLKJ <iftcceo@139.com>
2025-06-01 15:05:43 +0800

    Update index.js

--- static/user/index.js
+++ static/user/index.js
@@ -1,1 +1,12 @@
-if (!localStorage.getItem("ID")) location.href = "/login";
+const userId = localStorage.getItem("ID");
+if (!userId) location.href = "/login";
+if (!parserUrlParams().id) location.href = `/user?id=${userId}`;
+
+function parserUrlParams() {
+    const params = new URLSearchParams(location.search);
+    const result = {};
+    for (const [key, value] of params.entries()) {
+        result[key] = value;
+    }
+    return result;
+}
