05dd04e09662844ebf43c22a4e271c1e78a00456
IFTC-XLKJ <iftcceo@139.com>
2025-06-02 09:39:19 +0800

    Update vvcomponent.js

--- static/noob/editor/blocks/vvcomponent.js
+++ static/noob/editor/blocks/vvcomponent.js
@@ -4,7 +4,7 @@ Blockly.defineBlocksWithJsonArray([
         message0: "拖拽器 元素选择器%1",
         args0: [
             {
-                type: "field_input",
+                type: "input_value",
                 name: "selector",
                 check: "String"
             }
@@ -17,6 +17,9 @@ Blockly.defineBlocksWithJsonArray([
 ]);
 
 Blockly.JavaScript.forBlock["vv_dragger"] = function (block) {
-    const selector = block.getFieldValue('selector');
+    const selector = Blockly.JavaScript.valueToCode(block, "selector", Blockly.JavaScript.ORDER_ATOMIC);
+    if (!selector) {
+        throw new Error("Selector is required for the dragger block.");
+    }
     return [`(new Dragger(document.querySelector("${selector}")))`, Blockly.JavaScript.ORDER_ATOMIC];
 }
