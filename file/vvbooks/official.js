3c38dd50514558077d06d194daeb0accfcc8eee5
IFTC-XLKJ <151902522+IFTC-XLKJ@users.noreply.github.com>
2025-06-03 07:37:31 +0800

    update file/vvbooks/official.js

--- file/vvbooks/official.js
+++ file/vvbooks/official.js
@@ -2,7 +2,7 @@ class 官方扩展 extends Ext {
     constructor(name) {
         super(name);
         console.log("官方扩展加载成功", name);
-        console.log("访问", globalThis.localStorage, globalThis.parent);
+        console.log("访问", globalThis.localStorage, globalThis.parent, window);
     }
     onmessage(data) {
         console.log("官方扩展 onmessage", data);
