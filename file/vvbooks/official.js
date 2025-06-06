a42f2214a5e1e19c546fcba3a94e5d4b5b724967
IFTC-XLKJ <iftcceo@139.com>
2025-06-06 22:29:43 +0800

    Update official.js

--- file/vvbooks/official.js
+++ file/vvbooks/official.js
@@ -3,6 +3,9 @@ class 官方扩展 extends Ext {
         super(name);
         console.log("官方扩展加载成功", name);
         console.log("访问", iftc);
+        this.postMessage = (data) => {
+            console.log("官方扩展 postMessage", data);
+        }
     }
     onmessage(data) {
         console.log("官方扩展 onmessage", data);
