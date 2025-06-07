46011ec794c8764f5a2b549d68345e414ab4bc7a
IFTC-XLKJ <iftcceo@139.com>
2025-06-07 15:17:04 +0800

    Update document.js

--- static/noob/editor/blocks/document.js
+++ static/noob/editor/blocks/document.js
@@ -54,6 +54,37 @@ Blockly.defineBlocksWithJsonArray([
         helpUrl: "",
         output: "Dictionary",
         inputsInline: true
+    },
+    {
+        type: "get_document_element_by_class",
+        message0: "获取通过类元素 %1",
+        args0: [
+            {
+                type: "input_value",
+                name: "CLASS",
+                check: "String"
+            }
+        ],
+        colour: "#9C004B",
+        tooltip: "获取通过类元素",
+        helpUrl: "",
+        output: "Dictionary",
+    },
+    {
+        type: "get_document_body",
+        message0: "获取文档主体",
+        colour: "#9C004B",
+        tooltip: "获取文档主体",
+        helpUrl: "",
+        output: "Dictionary",
+    },
+    {
+        type: "get_document_head",
+        message0: "获取文档头部",
+        colour: "#9C004B",
+        tooltip: "获取文档头部",
+        helpUrl: "",
+        output: "Dictionary",
     }
 ])
 Blockly.JavaScript.forBlock['get_document'] = function (block) {
@@ -68,5 +99,23 @@ Blockly.JavaScript.forBlock['get_document_element'] = function (block) {
 Blockly.JavaScript.forBlock['get_document_elements'] = function (block) {
     var element = Blockly.JavaScript.valueToCode(block, 'ELEMENT', Blockly.JavaScript.ORDER_ATOMIC);
     var code = `document.getElementsByClassName(${element})`;
+    return [code, Blockly.JavaScript.ORDER_NONE];
+}
+Blockly.JavaScript.forBlock['get_document_element_by_id'] = function (block) {
+    var id = Blockly.JavaScript.valueToCode(block, 'ID', Blockly.JavaScript.ORDER_ATOMIC);
+    var code = `document.getElementById(${id})`;
+    return [code, Blockly.JavaScript.ORDER_NONE];
+}
+Blockly.JavaScript.forBlock['get_document_element_by_class'] = function (block) {
+    var className = Blockly.JavaScript.valueToCode(block, 'CLASS', Blockly.JavaScript.ORDER_ATOMIC);
+    var code = `document.getElementsByClassName(${className})[0]`;
+    return [code, Blockly.JavaScript.ORDER_NONE];
+}
+Blockly.JavaScript.forBlock['get_document_body'] = function (block) {
+    var code = 'document.body';
+    return [code, Blockly.JavaScript.ORDER_NONE];
+}
+Blockly.JavaScript.forBlock['get_document_head'] = function (block) {
+    var code = 'document.head';
     return [code, Blockly.JavaScript.ORDER_NONE];
 }
