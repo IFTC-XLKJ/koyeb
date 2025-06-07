b7e6d6401674f1389a279d9b4432746710e90db0
IFTC-XLKJ <iftcceo@139.com>
2025-06-07 15:14:16 +0800

    Update document.js

--- static/noob/editor/blocks/document.js
+++ static/noob/editor/blocks/document.js
@@ -6,6 +6,38 @@ Blockly.defineBlocksWithJsonArray([
         tooltip: "获取文档",
         helpUrl: "",
         output: "Dictionary",
+    },
+    {
+        type: "get_document_element",
+        message0: "获取元素 %1",
+        args0: [
+            {
+                type: "input_value",
+                name: "ELEMENT",
+                check: "String"
+            }
+        ],
+        colour: "#9C004B",
+        tooltip: "获取元素",
+        helpUrl: "",
+        output: "Dictionary",
+        inputsInline: true
+    },
+    {
+        type: "get_document_elements",
+        message0: "获取元素列表 %1",
+        args0: [
+            {
+                type: "input_value",
+                name: "ELEMENT",
+                check: "String"
+            }
+        ],
+        colour: "#9C004B",
+        tooltip: "获取元素列表",
+        helpUrl: "",
+        output: "Array",
+        inputsInline: true
     }
 ])
 Blockly.JavaScript.forBlock['get_document'] = function (block) {
