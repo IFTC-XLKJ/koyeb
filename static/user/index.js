21ac7dcf9938c77ab579371932b1b7c522a95b75
IFTC-XLKJ <iftcceo@139.com>
2025-06-15 10:50:48 +0800

    Update index.js

--- static/user/index.js
+++ static/user/index.js
@@ -31,34 +31,34 @@ updateUsername.addEventListener("click", async () => {
         }
     })
 });
-dialog.on("onInputFinish", async (value, dialogId) => {
-    console.log("onInputFinish", value, dialogId);
-    if (dialogId == "update-username") {
-        if (value.trim() == "") {
-            toast.showToast("用户名不能为空", 2, "center", "large", "error", "", true);
-            return;
-        }
-        const loadid = toast.loading("修改用户名中...");
-        try {
-            const response = await fetch(`/api/user/update?type=nickname&id=${userId}&password=${encodeURIComponent(password)}&data=${encodeURIComponent(value)}`);
-            const data = await response.json();
-            if (data.code == 200) {
-                toast.hideToast(loadid);
-                toast.showToast("修改用户名成功", 2, "center", "large", "success", "", false);
-                setTimeout(() => {
-                    location.reload();
-                }, 2000);
-            } else {
-                toast.hideToast(loadid);
-                toast.showToast("修改用户名失败，原因：" + data.msg, 2, "center", "large", "error", "", true);
-            }
-        } catch (e) {
-            toast.hideToast(loadid);
-            toast.showToast("修改用户名失败，原因：" + e, 2, "center", "large", "error", "", true);
-            return;
-        }
-    }
-});
+// dialog.on("onInputFinish", async (value, dialogId) => {
+//     console.log("onInputFinish", value, dialogId);
+//     if (dialogId == "update-username") {
+//         if (value.trim() == "") {
+//             toast.showToast("用户名不能为空", 2, "center", "large", "error", "", true);
+//             return;
+//         }
+//         const loadid = toast.loading("修改用户名中...");
+//         try {
+//             const response = await fetch(`/api/user/update?type=nickname&id=${userId}&password=${encodeURIComponent(password)}&data=${encodeURIComponent(value)}`);
+//             const data = await response.json();
+//             if (data.code == 200) {
+//                 toast.hideToast(loadid);
+//                 toast.showToast("修改用户名成功", 2, "center", "large", "success", "", false);
+//                 setTimeout(() => {
+//                     location.reload();
+//                 }, 2000);
+//             } else {
+//                 toast.hideToast(loadid);
+//                 toast.showToast("修改用户名失败，原因：" + data.msg, 2, "center", "large", "error", "", true);
+//             }
+//         } catch (e) {
+//             toast.hideToast(loadid);
+//             toast.showToast("修改用户名失败，原因：" + e, 2, "center", "large", "error", "", true);
+//             return;
+//         }
+//     }
+// });
 const updateAvatar = document.getElementById("update-avatar");
 updateAvatar.addEventListener("click", e => {
     const fileSelector = document.createElement("input");
