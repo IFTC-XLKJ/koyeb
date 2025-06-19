4a29b5eb929b25aefce571bb007f91b7c431e94b
IFTC-XLKJ <151902522+IFTC-XLKJ@users.noreply.github.com>
2025-06-19 21:16:53 +0800

    update file/vvbooks/official-ai.js

--- file/vvbooks/official-ai.js
+++ file/vvbooks/official-ai.js
@@ -36,8 +36,9 @@ class 官方AI extends Ext {
                 }
             }catch(e) {}
             const result0 = await responseClone.text();
-            const result = result0.split("\n\n");
-            result.forEach((item, index) => {
+            const result1 = result0.split("\n\n");
+            const result = [];
+            result1.forEach((item, index) => {
                 if (item == "data: [DONE]") return;
                 result[index] = JSON.parse(item.replace("data: "));
             });
