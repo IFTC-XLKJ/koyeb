31866044ec4a7a8097b380256e00ca4946803e61
IFTC-XLKJ <151902522+IFTC-XLKJ@users.noreply.github.com>
2025-06-19 21:08:17 +0800

    update file/vvbooks/official-ai.js

--- file/vvbooks/official-ai.js
+++ file/vvbooks/official-ai.js
@@ -2,7 +2,7 @@ class 官方AI extends Ext {
     api = "https://api.chatanywhere.tech/v1/chat/completions";
     key = "sk-p43eRgJpYgCqweXqUQczzjfvc0d1DIqwq9hUU1kCuV2LNmJv";
     model = "gpt-3.5-turbo";
-    systemPrompt = `你是`;
+    systemPrompt = `你是VV助手`;
     constructor(name) {
         super(name);
         console.log("官方AI 扩展 准备就绪！");
