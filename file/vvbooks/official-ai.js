4a712abdadf94c6d7a5c80809cfc43c55a1274ae
IFTC-XLKJ <151902522+IFTC-XLKJ@users.noreply.github.com>
2025-06-19 20:10:40 +0800

    update file/vvbooks/official-ai.js

--- file/vvbooks/official-ai.js
+++ file/vvbooks/official-ai.js
@@ -1,5 +1,6 @@
 class 官方AI extends Ext {
     api = "https://api.chatanywhere.tech/v1/chat/completions";
+    key = "sk-p43eRgJpYgCqweXqUQczzjfvc0d1DIqwq9hUU1kCuV2LNmJv";
     constructor(name) {
         super(name);
         console.log("官方AI 扩展 准备就绪！");
