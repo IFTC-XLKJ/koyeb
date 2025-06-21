7df1f6d77c0fa031d327d3726b1bf900468d126d
IFTC-XLKJ <iftcceo@139.com>
2025-06-21 08:48:37 +0800

    Update index.js

--- static/cloudfun/index.js
+++ static/cloudfun/index.js
@@ -102,12 +102,13 @@ update.addEventListener("click", async () => {
                     labels.innerHTML += `<li class="uuid">${item.UUID}</li>`;
                     Console.innerHTML += `<ul iftc-uuid="${item.UUID}" class="console${index == 0 ? " current-console" : ""}"></ul>`;
                     ws.onclose = () => {
+                        const consoleElement = Console.querySelector(`[iftc-uuid="${item.UUID}"]`);
                         console.log("WebSocket connection closed for UUID:", item.UUID);
-                            consoleElement.innerHTML += `<li style="color: red;">连接已断开，请刷新重试。</li>`;
-                            consoleElement.scrollTop = consoleElement.scrollHeight;
+                        consoleElement.innerHTML += `<li style="color: red;">连接已断开，请刷新重试。</li>`;
+                        consoleElement.scrollTop = consoleElement.scrollHeight;
                     };
                     ws.onmessage = e => {
-                    const consoleElement = Console.querySelector(`[iftc-uuid="${item.UUID}"]`);
+                        const consoleElement = Console.querySelector(`[iftc-uuid="${item.UUID}"]`);
                         const data = JSON.parse(e.data);
                         console.log(data, data.type);
                         if (data.type == "init") {
