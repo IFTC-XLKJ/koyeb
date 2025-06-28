7b14b968e2841cc008a16db1a8ada26d880f6531
IFTC-XLKJ <iftcceo@139.com>
2025-06-28 20:03:10 +0800

    Sanitize element code in addEventListener block
    Removes parentheses from the generated element code in the add_event_listener block to prevent syntax errors when generating JavaScript code from Blockly.

--- static/noob/editor/blocks/events.js
+++ static/noob/editor/blocks/events.js
@@ -47,5 +47,8 @@ Blockly.JavaScript.forBlock["add_event_listener"] = function (block) {
     const element = Blockly.JavaScript.valueToCode(block, "element", Blockly.JavaScript.ORDER_ATOMIC);
     const eventName = block.getFieldValue("eventName");
     const func = Blockly.JavaScript.valueToCode(block, "func", Blockly.JavaScript.ORDER_ATOMIC);
-    return `${element}.addEventListener("${eventName}", ${func});\n`;
+    return `${element
+        .replaceAll("(", "")
+        .replaceAll(")", "")
+    }.addEventListener("${eventName}", ${func});\n`;
 };
