5a522b5a0ed8c0f1e563ec30c9aec409cf80a9eb
IFTC-XLKJ <iftcceo@139.com>
2025-06-28 14:33:14 +0800

    Add full corner shape to default MDUI theme
    Introduces the '--mdui-shape-corner-full' property with a value of '1000rem' to the default MDUI theme configuration.

--- static/noob/editor/blocks/mdui.js
+++ static/noob/editor/blocks/mdui.js
@@ -93,6 +93,7 @@ Blockly.JavaScript.forBlock["mdui_theme_default"] = function (block) {
     const defaultTheme = {
         "--mdui-color-primary": "0, 192, 255",
         "--mdui-color-on-primary": "255, 255, 255, 0.4",
+        "--mdui-shape-corner-full": "1000rem",
     };
     return `mduiTheme(${JSON.stringify(defaultTheme, null, 4)})`;
 }
