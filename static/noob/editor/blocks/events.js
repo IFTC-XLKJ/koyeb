f4ddf8bf21ae64244f0b723be500604bf95d2e9a
IFTC-XLKJ <iftcceo@139.com>
2025-06-28 19:18:30 +0800

    Update event block options and fix eventName codegen
    Replaces the 'click' dropdown label with its Chinese equivalent and adds a 'load' event option. Also updates code generation to wrap eventName in quotes for correct JavaScript output.

--- static/noob/editor/blocks/events.js
+++ static/noob/editor/blocks/events.js
@@ -12,11 +12,12 @@ Blockly.defineBlocksWithJsonArray([
                 type: "field_dropdown",
                 name: "eventName",
                 options: [
-                    ["click", "click"],
+                    ["点击", "click"],
                     ["mouseover", "mouseover"],
                     ["mouseout", "mouseout"],
                     ["mousemove", "mousemove"],
                     ["mousedown", "mousedown"],
+                    ["load", "load"]
                 ]
             },
             {
@@ -36,5 +37,5 @@ Blockly.JavaScript.forBlock["add_event_listener"] = function (block) {
     const element = Blockly.JavaScript.valueToCode(block, "element", Blockly.JavaScript.ORDER_ATOMIC);
     const eventName = Blockly.JavaScript.valueToCode(block, "eventName", Blockly.JavaScript.ORDER_ATOMIC);
     const func = Blockly.JavaScript.valueToCode(block, "func", Blockly.JavaScript.ORDER_ATOMIC);
-    return `${element}.addEventListener(${eventName}, ${func});\n`;
+    return `${element}.addEventListener("${eventName}", ${func});\n`;
 };
