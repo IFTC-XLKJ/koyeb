1a8be1ef5a6b48353ebe4c9759a652ddc9ad0449
IFTC-XLKJ <iftcceo@139.com>
2025-07-19 14:04:25 +0800

    Update script.js

--- static/noob/editor/blocks/script.js
+++ static/noob/editor/blocks/script.js
@@ -277,7 +277,7 @@ Blockly.JavaScript.forBlock["window"] = function (block) {
 }
 
 Blockly.JavaScript.forBlock["fetch"] = function (block) { 
-    const url = Blockly.JavaScript.valueToCode(block, 'URL', Blockly.JavaScript.ORDER_ATOMIC);
+    const url = Blockly.JavaScript.valueToCode(block, 'url', Blockly.JavaScript.ORDER_ATOMIC);
     const method = Blockly.JavaScript.valueToCode(block, 'method', Blockly.JavaScript.ORDER_ATOMIC);
     const headers = Blockly.JavaScript.valueToCode(block, 'headers', Blockly.JavaScript.ORDER_ATOMIC);
     const body = Blockly.JavaScript.valueToCode(block, 'body', Blockly.JavaScript.ORDER_ATOMIC);
