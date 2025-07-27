a0596d965a9bf21b6f524e4711090fb6981a56d1
IFTC-XLKJ <iftcceo@139.com>
2025-07-27 11:36:31 +0800

    Update variables.js

--- static/noob/editor/blocks/variables.js
+++ static/noob/editor/blocks/variables.js
@@ -50,6 +50,35 @@ Blockly.defineBlocksWithJsonArray([
                 options: vars
             }
         ],
+    },
+    {
+        type: "variables_change",
+        message0: "将变量 %1 %2 %3",
+        args0: [
+            {
+                type: "field_vars",
+                name: "VARIABLE",
+                options: vars
+            },
+            {
+                type: "field_dropdown",
+                name: "OPERATOR",
+                options: [
+                    ["增加", "+="],
+                    ["减少", "-="],
+                    ["乘以", "*="],
+                    ["除以", "/="]
+                ]
+            },
+            {
+                type: "input_value",
+                name: "VALUE"
+            }
+        ],
+        inputsInline: true,
+        previousStatement: null,
+        nextStatement: null,
+        colour: "#FDBA54",
     }
 ])
 
