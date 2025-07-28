2764bf6b542e92ff8d623c6c6815933b6c8a79cb
IFTC-XLKJ <iftcceo@139.com>
2025-07-28 21:27:06 +0800

    Update custom-ext.js

--- static/noob/editor/custom-ext.js
+++ static/noob/editor/custom-ext.js
@@ -2,11 +2,11 @@ globalThis.loadCustomExt = async function (obj) {
     if (obj instanceof Object) {
         const { types } = obj;
         if (types instanceof Object) {
-            const { name, version, author, blocks } = types;
+            const { name, color, version, author, blocks } = types;
             let exit = false;
-            for(const content of toolbox.contents) {
+            for (const content of toolbox.contents) {
                 if (content.kind == "category" && content.name == name) {
-                    if(!confirm("是否覆盖 " + name + " ?")) {
+                    if (!confirm("是否覆盖 " + name + " ?")) {
                         exit = true;
                         break;
                     }
@@ -16,9 +16,10 @@ globalThis.loadCustomExt = async function (obj) {
             toolbox.contents.push({
                 kind: "category",
                 name: name,
+                colour: color,
                 contents: blocks
             });
-            workspace.updateToolbox(toolbox)
+            workspace.updateToolbox(toolbox);
         } else {
             alert("无法加载扩展，请检查扩展导出的格式是否正确");
         }
