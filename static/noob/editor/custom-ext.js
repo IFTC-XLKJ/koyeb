12319c3253b8749c2cbf527e376937f508dce98a
IFTC-XLKJ <iftcceo@139.com>
2025-07-29 12:39:39 +0800

    Update custom-ext.js

--- static/noob/editor/custom-ext.js
+++ static/noob/editor/custom-ext.js
@@ -82,10 +82,14 @@ globalThis.loadCustomExt = async function (obj) {
             newBlock.tooltip = block.tooltip;
             newBlock.helpUrl = block.helpUrl;
             newBlock.inputsInline = true;
-            if (!newBlock.output && newBlock.output != null) {
+            if (newBlock.output == void 0) {
                 newBlock.nextStatement = null;
                 newBlock.previousStatement = null;
             }
+            //  else if (newBlock.output != null) {
+            //     newBlock.nextStatement = null;
+            //     newBlock.previousStatement = null;
+            // }
             const params = block.params;
             let message = "";
             const args = [];
