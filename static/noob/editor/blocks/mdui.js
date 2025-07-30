e23b778ec26917f8ebdf07721e7d8cb8ea202200
IFTC-XLKJ <iftcceo@139.com>
2025-07-30 19:47:06 +0800

    Update mdui.js

--- static/noob/editor/blocks/mdui.js
+++ static/noob/editor/blocks/mdui.js
@@ -209,7 +209,7 @@ Blockly.JavaScript.forBlock["mdui_text_field"] = function (block) {
     const placeholder = block.getFieldValue("PLACEHOLDER");
     const helper = block.getFieldValue("HELPER");
     const value = block.getFieldValue("VALUE");
-    const maxlength = Blockly.JavaScript.valueToCode(block, "MAXLENGTH", Blockly.JavaScript.ORDER_ATOMIC);
+    const maxlength = Blockly.JavaScript.valueToCode(block, "MAX_LENGTH", Blockly.JavaScript.ORDER_ATOMIC);
     const type = block.getFieldValue("TYPE");
     return `<mdui-text-field${label ? ` label="${label}"` : ""}${placeholder ? ` placeholder="${placeholder}"` : ""}${helper ? ` helper="${helper}" helper-on-focus` : ""}${value ? ` value="${value}"` : ""}${maxlength ? ` maxlength="${maxlength}"` : ""} type="${type}"></mdui-text-field>`;
 }
