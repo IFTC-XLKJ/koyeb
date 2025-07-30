80ba0699f6289845d255bbeff543593cccdadaa3
IFTC-XLKJ <iftcceo@139.com>
2025-07-30 16:55:40 +0800

    Update mdui.js

--- static/noob/editor/blocks/mdui.js
+++ static/noob/editor/blocks/mdui.js
@@ -136,6 +136,7 @@ Blockly.JavaScript.forBlock["mdui_theme_default"] = function (block) {
         "--mdui-typescale-body-large-weight": "400",
         "--mdui-typescale-body-large-tracking": "0.01rem",
         "--mdui-typescale-body-large-line-height": "1.6rem",
+        "--mdui-color-on-surface": "0, 0, 0, 0.87"
     };
     return `mduiTheme(${JSON.stringify(defaultTheme, null, 4)})`;
 }
