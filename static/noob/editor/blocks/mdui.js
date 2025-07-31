4194d6bf548ff493c7cd705319037a213499a3d3
IFTC-XLKJ <iftcceo@139.com>
2025-07-31 20:39:14 +0800

    Update mdui.js

--- static/noob/editor/blocks/mdui.js
+++ static/noob/editor/blocks/mdui.js
@@ -278,6 +278,7 @@ Blockly.JavaScript.forBlock["mdui_theme_default"] = function (block) {
         "--mdui-color-outline": "121, 116, 126",
         "--mdui-color-on-surface-variant-light": "73, 69, 78",
         "--mdui-color-primary-container": "0 162 255",
+        "--mdui-color-background": "186 220 248",
     };
     return `mduiTheme(${JSON.stringify(defaultTheme, null, 4)})`;
 }
