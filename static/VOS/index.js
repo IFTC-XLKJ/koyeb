a87c1d47527f1da7d6a7fc7a16bd7f7d86b77f34
IFTC-XLKJ <iftcceo@139.com>
2025-08-05 13:54:31 +0800

    Update index.js

--- static/VOS/index.js
+++ static/VOS/index.js
@@ -1,11 +1,12 @@
-setTimeout(function () {
-    const loadingSrc = document.getElementById('waitLoad');
-    loadingSrc.style.display = "none";
-}, 200);
+
 
 async function init() {
     const db = new Dexie("VOS");
     console.log("Dexie:", db);
+    setTimeout(function () {
+        const loadingSrc = document.getElementById('waitLoad');
+        loadingSrc.style.display = "none";
+    }, 200);
 }
 
 init();
