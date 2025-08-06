d903c9b9f9cbb68427f45b28ef13d3e87841b653
IFTC-XLKJ <iftcceo@139.com>
2025-08-06 10:48:54 +0800

    Update index.js

--- static/VOS/index.js
+++ static/VOS/index.js
@@ -114,7 +114,7 @@ const appPath = "/data/apps/";
             });
             appBackstage.contentWindow.parent = null;
             const script = document.createElement("script");
-            script.innerText = `const parent = null;var parent = null;`;
+            script.innerText = `var parent = null;`;
             appBackstage.contentDocument.head.appendChild(script);
         });
         appBackstage.style.display = "none";
