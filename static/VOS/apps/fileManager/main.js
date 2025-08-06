61f5fcbb8f0f1d2eeba24f130a9f17a6461e11bb
IFTC-XLKJ <iftcceo@139.com>
2025-08-06 20:36:35 +0800

    Update main.js

--- static/VOS/apps/fileManager/main.js
+++ static/VOS/apps/fileManager/main.js
@@ -1,5 +1,7 @@
 console.log("start", parent, globalThis.parent, API);
 const { AppWindow } = API;
-const appWindow = new AppWindow();
+const appWindow = new AppWindow({
+    appid: "cn.iftc.filemanager",
+});
 appWindow.load(new URL("index.html", `inner-src:///data/apps/${API.appid}/`).toString().replaceAll("inner-src://", ""));
 API.createFile("/data/data/abc")
