c42590664d329c632622a263676431342fc48be2
IFTC-XLKJ <iftcceo@139.com>
2025-08-06 20:36:11 +0800

    666

--- static/VOS/apps/fileManager/main.js
+++ static/VOS/apps/fileManager/main.js
@@ -2,3 +2,4 @@ console.log("start", parent, globalThis.parent, API);
 const { AppWindow } = API;
 const appWindow = new AppWindow();
 appWindow.load(new URL("index.html", `inner-src:///data/apps/${API.appid}/`).toString().replaceAll("inner-src://", ""));
+API.createFile("/data/data/abc")
