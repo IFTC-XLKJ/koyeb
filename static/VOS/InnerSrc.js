6fdfa27fdead9fa782f26e0e4815525baa0dae32
IFTC-XLKJ <iftcceo@139.com>
2025-08-06 15:29:18 +0800

    Update InnerSrc.js

--- static/VOS/InnerSrc.js
+++ static/VOS/InnerSrc.js
@@ -1,9 +1,22 @@
 class InnerSrc extends HTMLElement {
+    src = "";
+    get src() {
+        return this.getAttribute("src");
+    }
+    set src(value) {
+        this.setAttribute("src", value);
+    }
     static get observedAttributes() {
         return ["src"];
     }
     constructor() {
         super();
+    }
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (oldValue === newValue) return;
+        if (name === "src") {
+            this.src = newValue;
+        }
     }
 }
 customElements.define("inner-src", InnerSrc);
