0646bfdfcea2039066cac504e39e48813f15af67
IFTC-XLKJ <iftcceo@139.com>
2025-08-07 14:17:30 +0800

    Update main.js

--- static/VOS/apps/fileManager/main.js
+++ static/VOS/apps/fileManager/main.js
@@ -11,5 +11,8 @@ appWindow.load(new URL("index.html", `inner-src:///data/apps/${API.appid}/`).toS
         new URL("index.js", `inner-src:///data/apps/${API.appid}/`).toString().replaceAll("inner-src://", "")
     ]
 });
+appWindow.onmessage = (data) => {
+    console.log(data);
+}
 const file = new API.File("/data/data/" + API.appid + "/abc.txt");
 file.create();
