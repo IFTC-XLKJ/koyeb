76f6d03dd8afc14d25eb130467d14da6134c9db9
IFTC-XLKJ <iftcceo@139.com>
2025-08-07 13:47:02 +0800

    666

--- static/VOS/apps/fileManager/main.js
+++ static/VOS/apps/fileManager/main.js
@@ -1,6 +1,8 @@
 console.log("start", parent, globalThis.parent, API);
 const { AppWindow } = API;
-const appWindow = new AppWindow();
+const appWindow = new AppWindow({
+    width: 1000,
+});
 appWindow.load(new URL("index.html", `inner-src:///data/apps/${API.appid}/`).toString().replaceAll("inner-src://", ""), {
     styles: [
         new URL("index.css", `inner-src:///data/apps/${API.appid}/`).toString().replaceAll("inner-src://", "")
