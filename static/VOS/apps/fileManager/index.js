6c08d15f06a53eab72a6ca82736623356fc84a10
IFTC-XLKJ <iftcceo@139.com>
2025-08-08 11:22:31 +0800

    Update index.js

--- static/VOS/apps/fileManager/index.js
+++ static/VOS/apps/fileManager/index.js
@@ -1,5 +1,12 @@
-const tabs = [];
+// const tabs = [];
 addEventListener("load", e => {
     console.log("fileManager DOMContentLoaded");
     API.postMessage({ type: "setDrag" });
+    // tabs.push({})
 })
+
+function newTab(name, path) {
+    const tab = document.createElement("div");
+    tab.className = "tab";
+    tab.innerHTML = `<span>${name}</span><button class="close">x</button>`;
+}
