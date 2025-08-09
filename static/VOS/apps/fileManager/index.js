3a07516bf2f87de59ab51d12cc1f91d239ab7bb8
IFTC-XLKJ <iftcceo@139.com>
2025-08-09 14:34:54 +0800

    8

--- static/VOS/apps/fileManager/index.js
+++ static/VOS/apps/fileManager/index.js
@@ -8,7 +8,7 @@ addEventListener("load", async e => {
     try {
         API.postMessage({ type: "setDrag" });
         newTab("Home", "/storage/share/");
-        renderFileList(await file.getFileList());
+        renderFileList(await new API.File("/storage/share/").getFileList());
         document.querySelector(".tab").classList.add("active");
         pathSpan.textContent = "/storage/share/";
         pathInput.value = pathSpan.textContent;
