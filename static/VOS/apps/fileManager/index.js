fb53b44329d5675e92e85e25386c355f97dc4342
IFTC-XLKJ <iftcceo@139.com>
2025-08-09 15:50:15 +0800

    2

--- static/VOS/apps/fileManager/index.js
+++ static/VOS/apps/fileManager/index.js
@@ -75,7 +75,7 @@ function renderFileList(list) {
         const row = document.createElement("tr");
         row.className = "folder";
         row.innerHTML = `<td class="name">${folder}</td><td class="type">文件夹</td>`;
-        row.addEventListener("click", async function () {
+        row.addEventListener("dblclick", async function () {
             renderFileList(await new API.File(path + folder + "/").getFileList());
         });
         fileList.appendChild(row);
