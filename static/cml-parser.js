bc98c534305adddc52514881dc8d69e5cd15388a
IFTC-XLKJ <iftcceo@139.com>
2025-09-28 07:57:24 +0800

    update static/cml-parser.js

--- static/cml-parser.js
+++ static/cml-parser.js
@@ -3,6 +3,9 @@
     globalThis.CMLParser = {};
     CMLParser.parser = function (CMLString) {
         const parser = new DOMParser();
-        const doc = parser.parseFromString(CMLString);
+        const doc = parser.parseFromString(CMLString, "application/xml");
+        console.log(doc);
+        return doc;
     }
 })();
+console.log(CMLParser.parser('<Text>114514</Text>').querySelector('Text'))
