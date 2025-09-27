472d9427114490e77c14f80d547e122ac6c192f8
IFTC-XLKJ <iftcceo@139.com>
2025-09-27 14:05:55 +0800

    66666666666666

--- static/blog/index.js
+++ static/blog/index.js
@@ -2,3 +2,6 @@ menu.addEventListener("click", function () {
     if (drawer.open) drawer.open = false;
     else drawer.open = true;
 });
+addEventListener("scroll", e => {
+    document.querySelector("mdui-top-app-bar").style.top = document.querySelector("html").scrollTop + "px";
+});
