d50577b980659bf5df3bc52d75bf1151006dd697
IFTC-XLKJ <iftcceo@139.com>
2025-09-27 17:01:10 +0800

    Update index.js

--- static/blog/index.js
+++ static/blog/index.js
@@ -8,3 +8,17 @@ addEventListener("scroll", e => {
 drawerHome.addEventListener("click", function () {
     window.location.href = "/";
 });
+const articles = document.getElementById("articles");
+function newArticle(title, date, desc, link) {
+    const articleCard = document.createElement("mdui-card");
+    articleCard.clickable = true;
+    articleCard.style.width = "90%";
+    articleCard.style.margin = "1rem auto";
+    articleCard.style.padding = "1rem";
+    articleCard.style.boxSizing = "border-box";
+    articleCard.style.cursor = "pointer";
+    articleCard.addEventListener("click", function () {
+        window.location.href = link;
+    });
+    articles.appendChild(articleCard);
+}
