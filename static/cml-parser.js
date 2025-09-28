f5912a044241e43d318337452b32c83427a3f94a
IFTC-XLKJ <iftcceo@139.com>
2025-09-28 17:17:48 +0800

    update static/cml-parser.js

--- static/cml-parser.js
+++ static/cml-parser.js
@@ -3,12 +3,16 @@
     const tags = [];
     globalThis.CMLParser = {};
     CMLParser.parser = function (CMLString) {
+        // 将CML解析为HTML
         const parser = new DOMParser();
         const doc = parser.parseFromString(CMLString, "application/xml");
         console.log(doc);
         const mainNodes = doc.children;
         for (let node in mainNodes) {}
         return doc;
+    }
+    CMLParser.register = function() {
+        
     }
 })();
 console.log(CMLParser.parser('<Text>114514</Text>').querySelector('Text'))
