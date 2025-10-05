004cc40b77f4be0bbc8b9bc549258f4d903284fc
IFTC-XLKJ <iftcceo@139.com>
2025-10-05 21:36:25 +0800

    Update index.js

--- static/webide_plugin/index.js
+++ static/webide_plugin/index.js
@@ -31,6 +31,12 @@ function addItem(plugin) {
     mainCard.appendChild(downloadBtn);
     pluginList.appendChild(mainCard);
 }
+(function () {
+    const p = downloading.shawdowRoot.querySelector(".panel");
+    p.style.position = "fixed";
+    p.style.left = "50%";
+    p.style.transform = "translate(-50%)";
+})();
 globalThis.pluginGetPage = 0;
 async function loadPlugin() {
     pluginGetPage++;
