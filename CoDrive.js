981f70516465cbe087f05e819b2fac8df52eae0d
IFTC-XLKJ <iftcceo@139.com>
2025-10-20 22:31:37 +0800

    update CoDrive.js

--- CoDrive.js
+++ CoDrive.js
@@ -1,7 +1,11 @@
 class CoDrive {
     baseUrl = 'https://drive.amethyst.ltd/api/v4';
     token = '';
-    
+    email = 'iftcceo@139.com';
+    pwd = 'stree123456';
+    constructor() {
+        
+    }
     fetchData(_raw) {
         const headers = new Headers();
         headers.append("Content-Type", "application/json");
