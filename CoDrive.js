bd9f14565bbfd825af926c88c8d9fd72deca9221
IFTC-XLKJ <iftcceo@139.com>
2025-10-21 21:26:54 +0800

    update CoDrive.js

--- CoDrive.js
+++ CoDrive.js
@@ -17,46 +17,73 @@ class CoDrive {
             const expires = json.data.token.access_expires;
             this.token = token;
             this.expires = Date.parse(expires);
-        }, 30000);
+        },
+            30000);
     }
-    async fetchData(path, method, _raw) {
+    async fetchData(path,
+        method,
+        _raw) {
         console.log(this.token);
         const headers = new Headers();
-        headers.append("Content-Type", "application/json");
-        headers.append("Authorization", `Bearer ${this.token}`)
+        headers.append("Content-Type",
+            "application/json");
+        headers.append("Authorization",
+            `Bearer ${this.token}`)
         const raw = _raw;
         console.log(raw);
         const requestOptions = {
             method: method,
             headers: headers,
-            body: method == 'GET' ? void 0 : raw,
+            body: method == 'GET' ? void 0: raw,
             redirect: 'follow'
         };
-        const r = await fetch(`${this.baseUrl}${path}`, requestOptions);
+        const r = await fetch(`${this.baseUrl}${path}`,
+            requestOptions);
         const j = await r.json();
-        console.log('CoDrive', j);
+        console.log('CoDrive',
+            j);
         return j;
     }
     start(app) {
-        app.get("/api/cloud/filelist", async (req,res) => {
-            const {uri,page,pagesize}=req.query;
-            const json = await this.fetchData(`/file?uri=${`cloudreve://my${decodeURIComponent(uri)}`}&page=${page}&page_size=${pagesize}`, "GET");
-            return res.json(json);
-        });
-        app.get("/api/cloud/createfile", async (req, res) => {
-            const {uri,type}=req.query;
-            const json = await this.createFile(uri, type);
-            return res.json(json);
-        });
+        app.get("/api/cloud/filelist",
+            async (req, res) => {
+                const {
+                    uri,
+                    page,
+                    pagesize
+                } = req.query;
+                const json = await this.fetchData(`/file?uri=${`cloudreve://my${decodeURIComponent(uri)}`}&page=${page}&page_size=${pagesize}`, "GET");
+                return res.json(json);
+            });
+        app.get("/api/cloud/createfile",
+            async (req, res) => {
+                const {
+                    uri,
+                    type
+                } = req.query;
+                const json = await this.createFile(uri, type);
+                return res.json(json);
+            });
     }
-    async createFile(uri, type) {
-        const json = await this.fetchData("/file/create", "POST", JSON.stringify({
-            uri: `cloudreve://my${uri}`,
-            type: type
-        }));
+    async createFile(uri,
+        type) {
+        const json = await this.fetchData("/file/create",
+            "POST",
+            JSON.stringify({
+                uri: `cloudreve://my${uri}`,
+                type: type
+            }));
         return json;
     }
-    async getFile(uri) {}
+    async getFile(uri) {
+        const json = await this.fetchData("/file/url",
+            "POST",
+            JSON.stringify({
+                uris: [
+                    `cloudreve://my${uri}`
+                ],
+            }));
+    }
 }
 
 module.exports = CoDrive;
