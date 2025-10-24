e25a7bd6cd0b9b6f37ad85d793349e429e762be9
IFTC-XLKJ <iftcceo@139.com>
2025-10-25 01:15:12 +0800

    update CoDrive.js

--- CoDrive.js
+++ CoDrive.js
@@ -128,7 +128,7 @@ class CoDrive {
         app.get('/api/cloud/avatar', async (req, res) => {
             const { key } = req.query;
             const j = await this.getFile(`/VVAvatar/${key}.vvavatar`);
-            if (j.error) return res.send();
+            if (j.error) return res.send(await fs.readFile(''));
             return res.send();
         });
     }
