d206c52d03801b484cb8b9d716f9a33029784bce
IFTC-XLKJ <iftcceo@139.com>
2025-10-25 14:58:59 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1740,7 +1740,7 @@ app.get("/api/user/search", async (req, res) => {
           ID: item.ID,
           username: String(item.昵称),
           avatar: item.头像,
-          VC: typeof item.V币 == "number" ? item.V币 : NaN,
+          VC: Number(item.V币),
           email: item.邮箱 || "Unknown Email",
           VIP: !!item.VIP,
           signed: item.签到 || null,
