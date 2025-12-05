5ea6299701d94c4868a12e111ea8f8347c160cbe
IFTC-XLKJ <iftcceo@139.com>
2025-12-05 21:19:22 +0800

    Update VVApps.js

--- VVApps.js
+++ VVApps.js
@@ -7,4 +7,44 @@ const getDataURL = "https://api.pgaot.com/dbs/cloud/get_table_data";
 const setDataURL = "https://api.pgaot.com/dbs/cloud/set_table_data";
 const contentType = "application/json";
 
-module.exports = class {};
+module.exports = class {
+    static async randomApps(platform) {
+        const j = await this.postData({
+            url: getDataURL,
+            key: VVAppsAppkey,
+            body: {
+                filter: `平台="${platform}"`,
+                page: 1,
+                limit: 10,
+                sort: "RANDOM()",
+            }
+        });
+        return j;
+    }
+    async postData(options = {}) {
+        const timestamp = Date.now();
+        const signaturePromise = sign.get(timestamp);
+        try {
+            const signature = await signaturePromise;
+            const response = await fetch(options.url, {
+                method: "POST",
+                headers: {
+                    "X-Pgaot-Key": options.key,
+                    "X-Pgaot-Sign": signature,
+                    "X-Pgaot-Time": timestamp.toString(),
+                    "Content-Type": contentType,
+                },
+                body: JSON.stringify(options.body),
+            })
+            if (!response.ok) {
+                throw new Error("Network response was not ok " + response.statusText);
+            }
+            const json = await response.json();
+            console.log(json);
+            return json;
+        } catch (error) {
+            console.error("There was a problem with the fetch operation:", error);
+            throw error;
+        }
+    }
+};
