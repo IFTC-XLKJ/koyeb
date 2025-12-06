1ff9ea00f3afc53d53eceaa2e93eb4829bc0bf5b
IFTC-XLKJ <iftcceo@139.com>
2025-12-06 21:27:56 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3064,7 +3064,15 @@ app.get("/api/vvapps/random", async (req, res) => {
   }
 });
 
-app.get("/api/vvapps/search", async (req, res) => { });
+app.get("/api/vvapps/search", async (req, res) => {
+  requestLog(req);
+  const { platform, keyword } = req.query;
+  if (!platform || !["android", "windows"].includes(platform) || !keyword) return res.status(400).json({
+    code: 400,
+    msg: "Invalid parameters",
+    timestamp: time()
+  });
+});
 
 app.listen(port, () => {
   startTime = Date.now();
