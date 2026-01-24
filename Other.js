f5d48d02873376552d3c16707ba12a17c90f978b
IFTC-XLKJ <iftcceo@139.com>
2026-01-24 20:29:58 +0800

    Update Other.js

--- Other.js
+++ Other.js
@@ -13,8 +13,14 @@ const maxmind = require('maxmind');
 const whois = require('whois');
 const { version } = require("os");
 const _CoDrive = require("./CoDrive.js");
+const { createClient } = require("@supabase/supabase-js");
 // console.log('初始化', CoDrive)
 // console.log(fs);
+
+const SUPABASE_URL = "https://dbmp-xbgmorqeur6oh81z.database.nocode.cn";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlzcyI6InN1cGFiYXNlIiwiaWF0IjoxNzQ2OTc5MjAwLCJleHAiOjE5MDQ3NDU2MDB9.11QbQ5OW_m10vblDXAlw1Qq7Dve5Swzn12ILo7-9IXY";
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 class Other {
   CoDrive = null;
