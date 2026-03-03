996e8a19c97966e03085ceffe4f63ab831f7406e
IFTC-XLKJ <iftcceo@139.com>
2026-03-03 23:30:29 +0800

    update tgbot.js

--- tgbot.js
+++ tgbot.js
@@ -5,7 +5,30 @@ const bot = new TelegramBot(token, {
   polling: true
 });
 
-
+bot.onText(/\/start/, (msg, match) => {
+  const chatId = msg.chat.id;
+  const btns = {
+    reply_markup: {
+      inline_keyboard: [
+        [ // 第一行
+          {
+            text: '访问官网',
+            url: 'https://iftc.koyeb.app' // 点击跳转链接
+          }, {
+            text: '获取帮助',
+            callback_data: 'help' // 点击后触发回调事件
+          }
+        ], [ // 第二行
+          {
+            text: '搜索内容',
+            switch_inline_query: '默认搜索词' // 点击后在当前对话框发起内联查询
+          }
+        ]
+      ]
+    }
+  };
+  bot.sendMessage(chatId, "欢迎使用VV助手", btns);
+})
 
 bot.onText(/\/hello/, function onLoveText(msg) {
   bot.sendMessage(msg.chat.id, 'Hello, Telegram!');
