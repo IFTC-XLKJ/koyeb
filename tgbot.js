31292525bcdca0c276e7e8e52dcf9881b6848ad8
IFTC-XLKJ <iftcceo@139.com>
2026-03-04 00:47:28 +0800

    update tgbot.js

--- tgbot.js
+++ tgbot.js
@@ -10,15 +10,13 @@ bot.onText(/\/start/, (msg, match) => {
   const btns = {
     reply_markup: {
       inline_keyboard: [
-        [
-          {
-            text: '访问官网',
-            url: 'https://iftc.koyeb.app'
-          }, {
-            text: '获取帮助',
-            callback_data: 'help'
-          }
-        ], []
+        [{
+          text: '访问官网',
+          url: 'https://iftc.koyeb.app'
+        }, {
+          text: '获取帮助',
+          callback_data: 'help'
+        }], []
       ]
     }
   };
@@ -41,18 +39,18 @@ bot.onText(/\/queryuser/, (msg, match) => {
   return bot.sendMessage(chatId, "请输入用户ID以查询用户信息");
 });
 bot.onText(/\/queryuser (.+)/, async (msg, match) => {
-  try{
-  const chatId = msg.chat.id;
-  const resp = match[1];
-  const ID = Number(resp);
-  if (ID.trim() == "") return bot.sendMessage(chatId, "请输入有效的用户ID，如：0");
-  if (isNaN(ID)) return bot.sendMessage(chatId, "请输入有效的用户ID，如：0");
-  if (ID < 0) return bot.sendMessage(chatId, "请输入有效的用户ID，如：0");
-  bot.sendMessage(chatId, "正在查询中，请稍后...");
-  const r = await fetch("https://iftc.koyeb.app/api/user/details?id=" + ID);
-  const j = await r.json();
-  bot.sendMessage(chatId, JSON.stringify(j, null, 4));
-  }.catch(error) {
+  try {
+    const chatId = msg.chat.id;
+    const resp = match[1];
+    const ID = Number(resp);
+    if (ID.trim() == "") return bot.sendMessage(chatId, "请输入有效的用户ID，如：0");
+    if (isNaN(ID)) return bot.sendMessage(chatId, "请输入有效的用户ID，如：0");
+    if (ID < 0) return bot.sendMessage(chatId, "请输入有效的用户ID，如：0");
+    bot.sendMessage(chatId, "正在查询中，请稍后...");
+    const r = await fetch("https://iftc.koyeb.app/api/user/details?id=" + ID);
+    const j = await r.json();
+    bot.sendMessage(chatId, JSON.stringify(j, null, 4));
+  } catch(error) {
     console.error('Promise Catch:', error);
     bot.sendMessage(chatId, "查询出错：" + error + "，请稍后再试...");
   }
