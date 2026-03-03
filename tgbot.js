e5e5fe7d0c1915bd55dabb5e0ca004a0e8c08cef
IFTC-XLKJ <iftcceo@139.com>
2026-03-03 22:30:09 +0800

    update tgbot.js

---
+++ tgbot.js
@@ -0,0 +1,20 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = '8201224672:AAHd8id6qYF4_J-vi4t7mcrewLN9qa1gKv4';
+const bot = new TelegramBot(token, {
+  polling: true
+});
+
+
+
+bot.onText(/\/hentai/, function onLoveText(msg) {
+  bot.sendMessage(msg.chat.id, 'Are you a hetai?');
+});
+
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+
+  const chatId = msg.chat.id;
+  const resp = match[1];
+  bot.sendMessage(chatId, resp);
+});
