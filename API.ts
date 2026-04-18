0f653502445af96ae5190b3f2b84fe5ce1d53f88
IFTC-XLKJ <iftcceo@139.com>
2026-04-18 17:03:57 +0800

    0

--- API.ts
+++ API.ts
@@ -25,7 +25,17 @@ export default function (fastify: FastifyInstance) {
         ): Promise<Object> => {
             const { id } = request.query;
             console.log(id, typeof id);
+            if (Number.isNaN(Number(id)))
+                return reply.status(200).send({
+                    code: 400,
+                    msg: "id参数类型错误，必须为数值类型",
+                    timestamp: time(),
+                });
             return { message: "Hello, World!" };
         },
     );
+}
+
+function time(): number {
+    return Date.now();
 }
