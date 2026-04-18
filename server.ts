aaa307433bac08883249053a86df0367f0b1b643
IFTC-XLKJ <iftcceo@139.com>
2026-04-18 12:31:30 +0800

    0

--- server.ts
+++ server.ts
@@ -1,13 +1,15 @@
-import Fastify from "fastify";
-const fastify = Fastify({
+import Fastify, { FastifyInstance } from "fastify";
+const fastify: FastifyInstance = Fastify({
     logger: true,
 });
+
+const port: number = Number(process.env.PORT) || 3000;
 
 fastify.get("/", async (request, reply) => {
     return { hello: "world" };
 });
 
-fastify.listen({ port: 3000 }, (err, address) => {
+fastify.listen({ port: port }, (err, address) => {
     if (err) {
         fastify.log.error(err);
         process.exit(1);
