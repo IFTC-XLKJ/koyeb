0f74655e6192775139bf69585b654ee0ec6eff4e
IFTC-XLKJ <iftcceo@139.com>
2026-04-18 19:04:04 +0800

    4

--- API.ts
+++ API.ts
@@ -1,8 +1,18 @@
 import type { FastifyInstance, FastifyReply, FastifyRequest, FastifyError } from "fastify";
 import type { GetByIDResponse, UserData } from "./types.ts";
 import User from "./User.ts";
+import { createClient } from "@supabase/supabase-js";
+import type { SupabaseClient } from "@supabase/supabase-js";
+import type { StorageClient } from "@supabase/storage-js";
+import { PostgrestClient } from "@supabase/postgrest-js";
 
-const user = new User();
+const user: User = new User();
+const SUPABASE_URL: string = "https://dbmp-xbgmorqeur6oh81z.database.nocode.cn";
+const SUPABASE_ANON_KEY: string =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlzcyI6InN1cGFiYXNlIiwiaWF0IjoxNzQ2OTc5MjAwLCJleHAiOjE5MDQ3NDU2MDB9.11QbQ5OW_m10vblDXAlw1Qq7Dve5Swzn12ILo7-9IXY";
+const supabase: SupabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const avatarBucket = supabase.storage.from("avatar");
+const messagesTable = supabase.from("Messages");
 
 interface UserDetailsQueryParams {
     id: number;
@@ -12,6 +22,10 @@ interface UserMessagesQueryParams {
     id: number;
     start: number;
     end: number;
+}
+
+interface UserMessageItem {
+    createdAt: number;
 }
 
 export default function (fastify: FastifyInstance) {
@@ -123,8 +137,28 @@ export default function (fastify: FastifyInstance) {
         ): Promise<Object> => {
             const { id, start, end } = request.query;
             try {
-                
-            } catch (error:unknown) {
+                const { data, error } = await messagesTable
+                    .select()
+                    .eq("uid", id)
+                    .range(start - 1, end - 1)
+                    .order("createdAt", { ascending: false });
+                if (error)
+                    return reply.status(500).send({
+                        code: 500,
+                        msg: "Internal Server Error",
+                        error: error.message,
+                        timestamp: time(),
+                    });
+                return reply.send({
+                    code: 200,
+                    msg: "获取消息成功",
+                    data: data.map((item: UserMessageItem) => ({
+                        ...item,
+                        createdAt: Date.parse(String(item.createdAt)),
+                    })),
+                    timestamp: time(),
+                });
+            } catch (error: unknown) {
                 return reply.status(500).send({
                     code: 500,
                     msg: "服务器内部错误",
