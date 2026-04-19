c4e8a911899572b0f3a5cd7d5e8c8213f70ec4d2
IFTC-XLKJ <iftcceo@139.com>
2026-04-19 14:13:37 +0800

    8

--- tgbot.ts
+++ tgbot.ts
@@ -5,5 +5,27 @@ const bot = new TelegramBot(token, {
     polling: true,
 });
 
+bot.onText(/\/start/, (msg: TelegramBot.Message, match: any): Promise<TelegramBot.Message> => {
+    const chatId: number = msg.chat.id;
+    const btns: Object = {
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    {
+                        text: "访问官网",
+                        url: "https://iftc.koyeb.app",
+                    },
+                    {
+                        text: "获取帮助",
+                        callback_data: "help",
+                    },
+                ],
+                [],
+            ],
+        },
+    };
+    return bot.sendMessage(chatId, "欢迎使用VV助手", btns);
+});
+
 export default bot;
 export { TelegramBot };
