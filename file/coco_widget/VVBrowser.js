b78090bad3779a826f009d110c95ccdb10c5a9a3
IFTC-XLKJ <iftcceo@139.com>
2026-05-02 20:19:10 +0800

    1

--- file/coco_widget/VVBrowser.js
+++ file/coco_widget/VVBrowser.js
@@ -16,21 +16,37 @@ class Widget extends InvisibleWidget {
         super(props);
         Object.assign(this, props);
         this.widgetWarn("IFTC官网QQ群：870350184");
-    }
-}
+        const isVVBrowser = !!globalThis.isVVBrowser;
+        this.widgetWarn(isVVBrowser ? "当前环境为VV浏览器" : "当前环境非VV浏览器");
+        if (!isVVBrowser) globalThis.vvbrowser = {};
 
-types['methods'].push({
-    key: 'isVVBrowser',
-    label: '是否在VV浏览器中',
-    params: [],
-    blockOptions: {
-        callMethodLabel: false,
-        color: METHOD_COLOR,
-    },
-    valueType: 'boolean',
-})
-Widget.prototype.isVVBrowser = function () {
-    return !!globalThis.isVVBrowser;
+        types['methods'].push({
+            key: 'isVVBrowser',
+            label: '是否在VV浏览器中',
+            params: [],
+            blockOptions: {
+                callMethodLabel: false,
+                color: METHOD_COLOR,
+            },
+            valueType: 'boolean',
+        })
+        Widget.prototype.isVVBrowser = function () {
+            return isVVBrowser;
+        }
+        types['methods'].push({
+            key: 'getVVBrowserVersionName',
+            label: '获取VV浏览器版本',
+            params: [],
+            blockOptions: {
+                callMethodLabel: false,
+                color: METHOD_COLOR,
+            },
+            valueType: 'string',
+        })
+        Widget.prototype.getVVBrowserVersionName = function () {
+            return globalThis.isVVBrowser ? globalThis.vvbrowser.version.browser.name : 'UNKNOWN';
+        }
+    }
 }
 
 exports.types = types;
