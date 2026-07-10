257f3759db4131bbda476f6242573ec98747c588
IFTC-XLKJ <iftcceo@139.com>
2026-07-10 15:46:57 +0800

    6

--- API.ts
+++ API.ts
@@ -1131,6 +1131,9 @@ export default function (fastify: FastifyInstance) {
                 });
                 const data: string[][] = [];
                 (whoisData as string).split("\n").forEach((item) => {
+                    if (item.split(": ")[0].trim() && item.split(": ")[1].trim() == null) {
+                        return data.push([item.split(": ")[0].trim(), ""]);
+                    }
                     if (item.split(": ")[0].trim() && item.split(": ")[1].trim())
                         data.push([item.split(": ")[0].trim(), item.split(": ")[1].trim()]);
                 });
