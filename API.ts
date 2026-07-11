05031d3d19118282f68631b4c04446ee865b1ed2
IFTC-XLKJ <iftcceo@139.com>
2026-07-11 20:40:46 +0800

    0

--- API.ts
+++ API.ts
@@ -1373,7 +1373,7 @@ export default function (fastify: FastifyInstance) {
                         Origin: "https://iftc.koyeb.app",
                     },
                     body: JSON.stringify({
-                        model: "gpt-5.2",
+                        model: "gpt-oss-120b",
                         flashvider: "azureml",
                         temperature: 0.5,
                         top_p: 1,
