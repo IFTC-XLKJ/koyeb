98ffe43a17ee8243e3d73a5279592133e8790929
IFTC-XLKJ <iftcceo@139.com>
2026-08-05 14:22:28 +0800

    9

--- server.ts
+++ server.ts
@@ -863,13 +863,13 @@ process.on("uncaughtException", (err) => {
 });
 
 setInterval(async (): Promise<void> => {
-    // try {
-    //     await systemMonitor();
+    try {
+        await systemMonitor();
     //     const r: Response = await fetch("https://iftc.deno.dev");
     //     console.log(await r.text());
-    // } catch (e) {
-    //     console.error("Monitor error:", e);
-    // }
+    } catch (e) {
+        console.error("Monitor error:", e);
+    }
     const timeStr: string = new Date().toLocaleDateString("zh-CN", {
         year: "numeric",
         month: "long",
